343cdf868127ba6cd31e44b255a899f62aea03dd
Philipp Schubert <pdschbrt@mail.uni-paderborn.de>
2017-03-30 08:15:40 +0200

    revisited hexastores

--- src/db/Test.cpp
+++ src/db/Test.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
-#include <memory>
+#include <array>
+#include <algorithm>
 #include <assert.h>
 #include "Hexastore.hh"
-
 
 using namespace std;
 using namespace hexastore;
 
-int main() {
-
+int hs_test_main() {
   hexastore::Hexastore h("test.sqlite");
-
-  h.put((string[]){"mary", "likes", "hexastores"});
-  h.put((string[]){"mary", "likes", "apples"});
-  h.put((string[]){"peter", "likes", "apples"});
-  h.put((string[]){"peter", "hates", "hexastores"});
-
-  auto output = [](hs_result res) {
-    cout << res.subject << " " << res.predicate << " " << res.object << "\n";
-    return 0;
-  };
-
+  // init with some stuff
+  h.put({"mary", "likes", "hexastores"});
+  h.put({"mary", "likes", "apples"});
+  h.put({"peter", "likes", "apples"});
+  h.put({"peter", "hates", "hexastores"});
+  h.put({"frank", "admires", "bananas"});
+  //query some stuff
   cout << "Who likes what?" << "\n";
-  h.get((vector<string>){"?", "likes", "?"}, output);
+  auto result = h.get({"?", "likes", "?"});
+  for_each(result.begin(), result.end(), [](hs_result r){
+	  cout << r << endl;
+  });
   cout << "\n";
   cout << "What does peter hate?" << "\n";
-  h.get((vector<string>){"peter", "hates", "?"}, output);
-
-
-
+  result = h.get({"peter", "hates", "?"});
+  for_each(result.begin(), result.end(), [](hs_result r){
+	  cout << r << endl;
+  });
+  cout << "\n";
+  cout << "Who admires something?" << "\n";
+  result = h.get({"?", "admires", "?"});
+  for_each(result.begin(), result.end(), [](hs_result r){
+	  cout << r << endl;
+  });
   return 0;
 }
