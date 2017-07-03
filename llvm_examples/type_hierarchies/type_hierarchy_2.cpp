7d154213b2d878f69e907b8ca158e72282395a8b
Philipp Schubert <philipp.schubert@upb.de>
2017-07-03 09:21:51 +0200

    changed a few things

---
+++ llvm_examples/type_hierarchies/type_hierarchy_2.cpp
@@ -0,0 +1,14 @@
+struct Base {
+	virtual int foo() { return 1; }
+};
+
+struct Child : Base {
+	int foo() override { return 2; }
+	int bar() { return 3; }
+};
+
+int main()
+{
+	Child c;
+	return 0;
+}
