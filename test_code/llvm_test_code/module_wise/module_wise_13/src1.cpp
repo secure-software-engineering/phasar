c2ccf4ffa0a5c69d800d4f72994dadf2474aa2e8
linuxfan91 <linuxfan91@googlemail.com>
2018-02-22 07:45:26 +0100

    make test compile - to be reviewed please!

--- test_code/llvm_test_code/module_wise/module_wise_13/src1.cpp
+++ test_code/llvm_test_code/module_wise/module_wise_13/src1.cpp
@@ -1,15 +1,10 @@
 #include "src1.h"
 
-<<<<<<< HEAD
 int A::id(int i) {
 	return i;
 }
 
 
-int B::id(int i) {
-	return 0;
-}
-=======
 int A::foo(int &i) {
 	return i+10;
 }
@@ -17,4 +12,4 @@ int A::foo(int &i) {
 void A::bar(double &d) {
 	double a = 20.12 + d;
 }
->>>>>>> const-analysis
+
