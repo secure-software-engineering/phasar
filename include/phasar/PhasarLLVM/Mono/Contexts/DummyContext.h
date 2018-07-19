1e42a049b60147ff2c4c16b1eed1f9fe54d3ea3a
Nicolas Bellec <nicolas.bellec@ens-rennes.fr>
2018-07-19 14:03:42 +0200

    Working DummyContext

--- include/phasar/PhasarLLVM/Mono/Contexts/DummyContext.h
+++ include/phasar/PhasarLLVM/Mono/Contexts/DummyContext.h
@@ -17,21 +17,30 @@
 /*
  * Dummy context that does not stock anything if you do not need context.
  */
+#pragma once
 
-#include "ContextBase"
+#include <ostream>
 
-class DummmyContext : ContextBase<void, void, DummyContext> {
+#include "ContextBase.h"
+
+
+namespace psr {
+
+class DummyContext : ContextBase<void*, void*, DummyContext> {
 public:
-  using Node_t    = void;
-  using Domain_t  = void;
+  using Node_t    = ContextBase::Node_t;
+  using Domain_t  = ContextBase::Domain_t;
+  using ConcreteContext = DummyContext;
 
 public:
-  virtual void exitFunction(const Node_t src, const Node_t dest, const Domain_t &In) {};
-  virtual void enterFunction(const Node_t src, const Node_t dest, const Domain_t &In) {};
-  virtual bool isUnsure() { return true; };
+  virtual void exitFunction(const Node_t src, const Node_t dest, const Domain_t &In) override {};
+  virtual void enterFunction(const Node_t src, const Node_t dest, const Domain_t &In) override {};
+  virtual bool isUnsure() override { return true; };
 
-  virtual bool isEqual(const ConcreteContext &rhs) const { return true; };
-  virtual bool isDifferent(const ConcreteContext &rhs) const { return false; };
-  virtual bool isLessThan(const ConcreteContext &rhs) const { return false; };
-  virtual void print(std::ostream &os) const {};
+  virtual bool isEqual(const ConcreteContext &rhs) const override { return true; };
+  virtual bool isDifferent(const ConcreteContext &rhs) const override { return false; };
+  virtual bool isLessThan(const ConcreteContext &rhs) const override { return false; };
+  virtual void print(std::ostream &os) const override {};
 };
+
+} // namespace psr
