e0441d04725767da850f975aedd3282196ee104f
Richard Leer <rleer@mail.upb.de>
2018-09-28 09:39:12 +0200

    Add functions to map IR back to source code
    To map IR back to source code we are using debug information provided by
    the front end, i.e. IR file needs to be compiled with -g flag.
    Depending on the IR instruction a subset of the following source
    code information can be provided:
    - name of the variable
    - line and column
    - path to source file (.cpp/.c)
    - name of the enclosing function
    
    This information allows the analysis writer to generate more expressive
    reports at the end of an analysis run.

---
+++ unittests/Utils/LLVMIRToSrcTest.cpp
@@ -0,0 +1,95 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <llvm/IR/IntrinsicInst.h>
+#include <llvm/Support/raw_ostream.h>
+#include <phasar/DB/ProjectIRDB.h>
+#include <phasar/PhasarLLVM/ControlFlow/LLVMBasedICFG.h>
+#include <phasar/PhasarLLVM/Passes/ValueAnnotationPass.h>
+#include <phasar/PhasarLLVM/Pointer/LLVMTypeHierarchy.h>
+#include <phasar/Utils/LLVMIRToSrc.h>
+#include <phasar/Utils/LLVMShorthands.h>
+#include <phasar/Utils/Logger.h>
+
+using namespace psr;
+
+/* ============== TEST FIXTURE ============== */
+
+class LLVMIRToSrcTest : public ::testing::Test {
+protected:
+  const std::string pathToLLFiles =
+      PhasarDirectory + "build/test/llvm_test_code/llvmIRtoSrc/";
+
+  ProjectIRDB *IRDB;
+  LLVMTypeHierarchy *TH;
+  LLVMBasedICFG *ICFG;
+
+  LLVMIRToSrcTest() {}
+  virtual ~LLVMIRToSrcTest() {}
+
+  void Initialize(const std::vector<std::string> &IRFiles) {
+    IRDB = new ProjectIRDB(IRFiles);
+    IRDB->preprocessIR();
+    TH = new LLVMTypeHierarchy(*IRDB);
+    ICFG = new LLVMBasedICFG(*TH, *IRDB, CallGraphAnalysisType::OTF, {"main"});
+  }
+
+  void SetUp() override {
+    bl::core::get()->set_logging_enabled(false);
+    ValueAnnotationPass::resetValueID();
+  }
+
+  void TearDown() override {
+    delete IRDB;
+    delete TH;
+    delete ICFG;
+  }
+}; // Test Fixture
+
+TEST_F(LLVMIRToSrcTest, HandleInstructions) {
+  Initialize({pathToLLFiles + "function_call_cpp_dbg.ll"});
+  auto Fmain = ICFG->getMethod("main");
+  for (auto &BB : *Fmain) {
+    for (auto &I : BB) {
+      if (llvm::isa<llvm::StoreInst>(&I) ||
+          (llvm::isa<llvm::CallInst>(&I) &&
+           !llvm::isa<llvm::DbgValueInst>(&I) &&
+           !llvm::isa<llvm::DbgDeclareInst>(&I)) ||
+          llvm::isa<llvm::LoadInst>(&I)) {
+        std::cout << '\n'
+                  << llvmIRToString(&I) << "\n  --> "
+                  << llvmInstructionToSrc(&I) << std::endl;
+      }
+    }
+  }
+}
+
+TEST_F(LLVMIRToSrcTest, HandleFunctions) {
+  Initialize({pathToLLFiles + "multi_calls_cpp_dbg.ll"});
+  for (auto F : IRDB->getAllFunctions()) {
+    // F->print(llvm::outs());
+    // llvm::outs() << '\n';
+    std::cout << '\n' << llvmFunctionToSrc(F) << std::endl;
+  }
+}
+
+TEST_F(LLVMIRToSrcTest, HandleGlobalVariable) {
+  Initialize({pathToLLFiles + "global_01_cpp_dbg.ll"});
+  for (auto &GV :
+       IRDB->getModule(pathToLLFiles + "global_01_cpp_dbg.ll")->globals()) {
+    std::cout << '\n' << llvmGlobalValueToSrc(&GV) << std::endl;
+  }
+}
+
+TEST_F(LLVMIRToSrcTest, HandleAlloca) {
+  Initialize({pathToLLFiles + "function_call_cpp_dbg.ll"});
+  for (auto A : IRDB->getAllocaInstructions()) {
+    std::cout << '\n'
+              << llvmIRToString(A) << "\n  --> " << llvmValueToSrc(A)
+              << std::endl;
+  }
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
