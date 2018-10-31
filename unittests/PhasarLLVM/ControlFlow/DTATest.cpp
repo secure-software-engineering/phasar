79c5023825146fdc4e2981a69cf390bb2c4b01ec
Linus Jungemann <linusjun@mail.upb.de>
2018-10-31 14:28:03 +0100

    Add unitest classes for CHA,RTA,OTF,DTA

---
+++ unittests/PhasarLLVM/ControlFlow/DTATest.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <llvm/IR/InstIterator.h>
+#include <phasar/DB/ProjectIRDB.h>
+#include <phasar/PhasarLLVM/ControlFlow/LLVMBasedICFG.h>
+#include <phasar/Utils/LLVMShorthands.h>
+#include <phasar/PhasarLLVM/Pointer/LLVMTypeHierarchy.h>
+
+using namespace std;
+using namespace psr;
+
+class DTATest : public ::testing::Test {
+protected:
+  const std::string pathToLLFiles =
+      PhasarDirectory + "build/test/llvm_test_code/";
+};
+
+TEST_F(DTATest, test1)
+{
+    ASSERT_FALSE(true);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
