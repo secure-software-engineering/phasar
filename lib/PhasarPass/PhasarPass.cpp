e40c30b493c727eeebdc127ce9ec10f5a0ecb711
Philipp Schubert <philipp@it-schubert.com>
2019-02-08 07:35:08 +0100

    fixed path

--- lib/PhasarPass/PhasarPass.cpp
+++ lib/PhasarPass/PhasarPass.cpp
@@ -30,7 +30,7 @@
 #include <phasar/PhasarLLVM/IfdsIde/Solver/LLVMIFDSSolver.h>
 #include <phasar/PhasarLLVM/Mono/Problems/InterMonoSolverTest.h>
 #include <phasar/PhasarLLVM/Mono/Problems/InterMonoTaintAnalysis.h>
-#include <phasar/PhasarLLVM/Mono/Problems/InterMonotoneSolverTest.h>
+#include <phasar/PhasarLLVM/Mono/Problems/InterMonoSolverTest.h>
 #include <phasar/PhasarLLVM/Mono/Problems/IntraMonoFullConstantPropagation.h>
 #include <phasar/PhasarLLVM/Mono/Problems/IntraMonoSolverTest.h>
 #include <phasar/PhasarLLVM/Mono/Problems/IntraMonotoneSolverTest.h>
