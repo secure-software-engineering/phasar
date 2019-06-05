c6af4c079aa534c10ccc6092512015a8fdeac99c
Philipp Schubert <philipp@it-schubert.com>
2019-06-05 09:52:28 +0200

    added mono test cases

---
+++ test/llvm_test_code/taint_analysis/taint_11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int quk(int i) { return i; }
+
+int baz(int i) { return quk(i); }
+
+int bar(int i) { return baz(i); }
+
+int foo(int i) { 
+	return bar(i);
+}
+
+int main(int argc, char **argv) {
+	int y = foo(argc);
+	int a = 42;
+	int z = foo(a);
+	printf("%d\n", y);
+	printf("%d\n", z);
+}
