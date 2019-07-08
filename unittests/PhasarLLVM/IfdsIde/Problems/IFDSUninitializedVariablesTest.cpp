632fb7666dd05762592d647b699799aa9c3d1dbe
Fabian Schiebel <fabianbs@mail.upb.de>
2019-07-08 14:34:57 +0200

    Added some unittests; some of them fail (depending of definition of preciseness) need to check

--- unittests/PhasarLLVM/IfdsIde/Problems/IFDSUninitializedVariablesTest.cpp
+++ unittests/PhasarLLVM/IfdsIde/Problems/IFDSUninitializedVariablesTest.cpp
@@ -2,6 +2,7 @@
 #include <phasar/DB/ProjectIRDB.h>
 #include <phasar/PhasarLLVM/ControlFlow/LLVMBasedICFG.h>
 //#include <phasar/PhasarLLVM/IfdsIde/Problems/IFDSTaintAnalysis.h>
+#include <llvm/IR/Module.h>
 #include <phasar/PhasarLLVM/IfdsIde/Problems/IFDSUninitializedVariables.h>
 #include <phasar/PhasarLLVM/IfdsIde/Solver/LLVMIFDSSolver.h>
 #include <phasar/PhasarLLVM/Passes/ValueAnnotationPass.h>
@@ -108,7 +109,7 @@ TEST_F(IFDSUninitializedVariablesTest, UninitTest_03_SHOULD_LEAK) {
   GroundTruth[16] = {"9"};
   // The same as in test2: is it necessary to report again? (the analysis does
   // not)
-  GroundTruth[17] = {"16"};
+  // GroundTruth[17] = {"16"};
 
   compareResults(GroundTruth);
 }
@@ -145,12 +146,143 @@ TEST_F(IFDSUninitializedVariablesTest, UninitTest_06_SHOULD_NOT_LEAK) {
 TEST_F(IFDSUninitializedVariablesTest, UninitTest_07_SHOULD_LEAK) {
   Initialize({pathToLLFiles + "struct_member_uninit2_cpp_dbg.ll"});
   LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
-                                                              true, true);
+                                                              false, false);
   Solver.solve();
   // struct_member_uninit2.cpp contains a use of the uninitialized field _x.b
   map<int, set<string>> GroundTruth;
   // %5 = load i16, i16* %4; %4 is the uninitialized struct-member _x.b
   GroundTruth[4] = {"3"};
+
+  compareResults(GroundTruth);
+}
+
+TEST_F(IFDSUninitializedVariablesTest, UninitTest_08_SHOULD_NOT_LEAK) {
+  Initialize({pathToLLFiles + "global_variable_cpp_dbg.ll"});
+  LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
+                                                              false, false);
+  Solver.solve();
+  // global_variable.cpp does not contain undef-uses
+  map<int, set<string>> GroundTruth;
+  compareResults(GroundTruth);
+}
+
+TEST_F(IFDSUninitializedVariablesTest, UninitTest_09_SHOULD_LEAK) {
+  Initialize({pathToLLFiles + "global_variable_cpp_dbg.ll"});
+  LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
+                                                              false, false);
+  Solver.solve();
+  // global_variable.cpp does not contain undef-uses
+  map<int, set<string>> GroundTruth;
+  // load i32, i32* @i ; @i is uninitialized in the c++ code, but initialized in
+  // the LLVM-IR
+  GroundTruth[5] = {"0"};
+  compareResults(GroundTruth);
+}
+
+TEST_F(IFDSUninitializedVariablesTest, UninitTEst_10_SHOULD_LEAK) {
+  Initialize({pathToLLFiles + "return_uninit_cpp_dbg.ll"});
+  LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
+                                                              false, false);
+  Solver.solve();
+  map<int, set<string>> GroundTruth;
+  //%2 = load i32, i32 %1
+  GroundTruth[2] = {"0"};
+  // What about this call?
+  // %3 = call i32 @_Z3foov()
+  // GroundTruth[8] = {""};
+  compareResults(GroundTruth);
+}
+
+TEST_F(IFDSUninitializedVariablesTest, UninitTest_11_SHOULD_NOT_LEAK) {
+
+  Initialize({pathToLLFiles + "sanitizer_cpp_dbg.ll"});
+  LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
+                                                              false, false);
+  Solver.solve();
+  // all undef-uses are sanitized
+  map<int, set<string>> GroundTruth;
+  compareResults(GroundTruth);
+}
+TEST_F(IFDSUninitializedVariablesTest, UninitTest_12_SHOULD_LEAK) {
+
+  Initialize({pathToLLFiles + "sanitizer_uninit_cpp_dbg.ll"});
+  LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
+                                                              false, false);
+  Solver.solve();
+  // The sanitized value is not used always => the phi-node is "tainted"
+  map<int, set<string>> GroundTruth;
+  // GroundTruth[6] = {"2"};
+  // GroundTruth[13] = {"2"};
+  // GroundTruth[15] = {"13"};
+  GroundTruth[16] = {"15"};
+  compareResults(GroundTruth);
+}
+TEST_F(IFDSUninitializedVariablesTest, UninitTest_13_SHOULD_NOT_LEAK) {
+
+  Initialize({pathToLLFiles + "sanitizer2_cpp_dbg.ll"});
+  LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
+                                                              false, false);
+  Solver.solve();
+  // The undef-uses do not affect the program behaviour
+  map<int, set<string>> GroundTruth;
+  // GroundTruth[9] = {"2"};
+  compareResults(GroundTruth);
+}
+TEST_F(IFDSUninitializedVariablesTest, UninitTest_14_SHOULD_LEAK) {
+
+  Initialize({pathToLLFiles + "uninit_c_dbg.ll"});
+  LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
+                                                              false, false);
+  Solver.solve();
+  map<int, set<string>> GroundTruth;
+  GroundTruth[14] = {"1"};
+  GroundTruth[15] = {"2"};
+  GroundTruth[16] = {"14", "15"};
+  compareResults(GroundTruth);
+}
+
+TEST_F(IFDSUninitializedVariablesTest, UninitTest_15_SHOULD_LEAK) {
+  Initialize({pathToLLFiles + "dyn_mem_cpp_dbg.ll"});
+  LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
+                                                              false, false);
+  Solver.solve();
+  map<int, set<string>> GroundTruth;
+  // TODO remove GT[14] and GT[13]
+  GroundTruth[14] = {"3"};
+  GroundTruth[13] = {"2"};
+  GroundTruth[15] = {"13", "14"};
+
+  GroundTruth[35] = {"4"};
+  GroundTruth[38] = {"35"};
+
+  GroundTruth[28] = {"2"};
+  GroundTruth[29] = {"3"};
+  GroundTruth[30] = {"28", "29"};
+
+  GroundTruth[33] = {"30"};
+
+  // Analysis detects false positive at %12:
+
+  // store i32* %3, i32** %6, align 8, !dbg !28
+  // %12 = load i32*, i32** %6, align 8, !dbg !29
+
+  compareResults(GroundTruth);
+}
+
+TEST_F(IFDSUninitializedVariablesTest, UninitTest_16_SHOULD_LEAK) {
+
+  Initialize({pathToLLFiles + "growing_example_cpp_dbg.ll"});
+  LLVMIFDSSolver<const llvm::Value *, LLVMBasedICFG &> Solver(*UninitProblem,
+                                                              false, true);
+  Solver.solve();
+
+  map<int, set<string>> GroundTruth;
+  // TODO remove GT[11]
+  GroundTruth[11] = {"0"};
+
+  GroundTruth[16] = {"2"};
+  GroundTruth[18] = {"16"};
+  GroundTruth[34] = {"24"};
 
   compareResults(GroundTruth);
 }
