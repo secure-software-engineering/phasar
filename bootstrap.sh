fa2690d160d85cde1b58909a5b58e23b710b283d
Fabian Schiebel <fabianbs@mail.uni-paderborn.de>
2019-08-22 14:44:51 +0200

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -e
 echo "installing phasar dependencies..."
 
-sudo apt-get install zliblg-dev libncurses5-dev sqlite3 libsqlite3-dev libmysqlcppconn-dev bear python3 doxygen graphviz python python-dev python-pip libxml2 libxml2-dev libncurses5-dev libncursesw5-dev swig
+sudo apt-get update
+sudo apt-get install zlib1g-dev libncurses5-dev sqlite3 libsqlite3-dev libmysqlcppconn-dev bear python3 doxygen graphviz python python-dev python3-pip libxml2 libxml2-dev libncurses5-dev libncursesw5-dev swig build-essential g++ cmake libz3 libz3-dev libedit-dev python-sphinx libomp-dev
+sudo pip install Pygments
+sudo pip install pyyaml
 # installing boost
 wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
 tar xvf boost_1_66_0.tar.gz
@@ -12,11 +16,13 @@ cd ..
 # installing LLVM
 ./utils/install-llvm-8.0.0.sh $(nproc) ./utils/
 # installing wllvm
-pip install wllvm
-sudo pip install wllvm
+sudo pip3 install wllvm
 
 echo "dependencies successfully installed"
 echo "build phasar..."
+
+git submodule init
+git submodule update
 
 export CC=/usr/local/bin/clang
 export CXX=/usr/local/bin/clang++
