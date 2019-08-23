eb64b2676469686efb8c837fd9df9289829ff502
kaustubhkelkar <kskelkar@mail.uni-paderborn.de>
2019-08-23 17:45:25 +0200

    Task: -bounds checking for l#u expressions - completeness check (every events from EVENTS should be used here somewhere) Status: Incomplete

--- CogniCrypt++/CrySL/Parser/lib/OrderTypeChecker.cpp
+++ CogniCrypt++/CrySL/Parser/lib/OrderTypeChecker.cpp
@@ -7,24 +7,57 @@
 namespace CCPP {
 // using namespace std;
 
-bool checkEvent(
+bool checkEventAndSymbols(
     CrySLParser::PrimaryContext *primaryContext,
     std::unordered_map<std::string, std::shared_ptr<Type>> &DefinedObjects) {
   bool result = true;
   if (!DefinedObjects.count(primaryContext->eventName->getText())) {
     std::cerr << Position(primaryContext)
               << ": event is not defined in EVENTS section" << std::endl;
-    return false;
+    result=false;
+    return result;
   }
-  return result;
+
+  if(primaryContext-> eventTop->getText() == '+' || 
+      primaryContext-> eventTop->getText() == '*' || 
+      primaryContext-> eventTop->getText() == '?'){
+          std:out <<Position(primaryContext)
+              << ": symbol is defined" << std::endl;
+        }
+  else{   std::cerr << Position(primaryContext)
+              << ": symbol is not defined" << std::endl;
+              result=false;
+        }
+    return result;
+}
+
+book checkBound(CrySLParser::UnorderedSymbolsContext *unorderedSymbolsContext,
+    std::unordered_map<std::string, std::shared_ptr<Type>> &DefinedObjects){
+        bool result=true;
+
+        if!(unorderedSymbolsContext->lower->Ident()){
+            
+        }
+         
+
+
+        return result;
+    }
+
+bool checkSymbols(
+    CrySLParser::PrimaryContext *primaryContext,
+    std::unordered_map<std::string, std::shared_ptr<Type>> &DefinedObjects) {
+   bool result= true;
+   if(!DefinedObject.count(primaryContext->elementTop))     
+
 }
 
 bool CrySLTypechecker::CrySLSpec::typecheck(CrySLParser::OrderContext *order) {
   bool result = true;
-  for (auto simord : order->orderSequence()->simpleOrder()) {
-    for (auto unordsym : simord->unorderedSymbols()) {
-      for (auto primcon : unordsym->primary()) {
-        result &= checkEvent(primcon, DefinedObjects);
+  for (auto simpleOrder : order->orderSequence()->simpleOrder()) {
+    for (auto unorderedSymbolsContext : simpleOrder->unorderedSymbols()) {
+      for (auto primaryContext : unorderedSymbolsContext->primary()) {
+        result &= checkEventAndSymbols(primaryContext, DefinedObjects);
       }
     }
   }
