9ff4adf3788bdbb6a4515eb23fda86a0ee2c0b10
ammad <ammad@mail.upb.de>
2019-09-05 00:27:30 +0200

    handeled factory and consumer function with index

--- CogniCrypt++/CrySL/lib/FormatConverter/EventConverter.cpp
+++ CogniCrypt++/CrySL/lib/FormatConverter/EventConverter.cpp
@@ -6,18 +6,44 @@
 
 namespace CCPP {
 
-void EventConverter::formatConverter(CrySLParser::EventsContext *eventCtx) {
-  EventConverter eventConverter;
-  Event eventObj;
-  std::vector<Object> objects;
-  std::vector<Event> events;
+	void EventConverter::formatConverter(CrySLParser::EventsContext *eventCtx) {
+	  EventConverter eventConverter;
+	  Event eventObj;
+	  std::vector<Object> objects;
+	  std::vector<Event> events;
 
-  for (auto event : eventCtx->eventsOccurence()) {
-    eventObj.eventName = event->eventName->getText();
-    events.push_back(eventObj);
+	  for (auto event : eventCtx->eventsOccurence()) {
+		eventObj.eventName = event->eventName->getText();
 
-    for (auto param : event->parametersList()->param()) {
-    }
-  }
-}
+		for (auto param : event->parametersList()->param()) {
+                  objects.emplace_back(param);	//creates object of type Object using parameterize constructor with param as a parameter and inserts it in vector
+		}
+
+		checkFactoryConsumerFunc(event, eventObj);
+
+		events.push_back(eventObj);
+	  }
+	}
+
+	void checkFactoryConsumerFunc(auto event, Event eventObj) {
+	  if (event->returnValue && event->returnValue->getText() == "this") {
+			eventObj.isFactoryFunction = true;
+			eventObj.factoryParamIdx = {-1};
+	  } 
+	  else {
+            eventObj.isConsumingFunction = true;
+
+            int index = 0;
+            std::set<double> consumerParamIdx;
+            for (auto param : event->parametersList()->param()) {
+				
+             if (param->getText() == "this") {
+					consumerParamIdx.insert(index);
+              }
+              index++;
+			}
+            eventObj.consumerParamIdx = consumerParamIdx;
+		}
+	}
+
 } // namespace CCPP
