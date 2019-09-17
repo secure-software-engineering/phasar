af3d3fc3ad929912102089ab9972b5d8cd8551fb
Fabian Schiebel <fabianbs@mail.uni-paderborn.de>
2019-09-17 11:04:47 +0200

    Set up unittests with cmake - not tested

---
+++ CogniCrypt++/unittests/CrySL/TypeCheckTest.cpp
@@ -0,0 +1,65 @@
+// Unit Test suite
+#include <Parser/CrySLParserEngine.h>
+#include <gtest/gtest.h>
+#include <iostream>
+
+using namespace CCPP;
+
+/* ============== TEST FIXTURE ============== */
+
+class TypeCheckTest : public ::testing::Test {
+
+protected:
+  const std::string pathToCrySLFiles = "../test/CrySL/";
+  const std::vector<std::string> EntryPoints = {"main"};
+
+  CrySLParserEngine *cryslparserengine;
+
+  TypeCheckTest() {}
+  virtual ~TypeCheckTest() {}
+
+  void Initialize(const std::vector<std::string> &CrySLFiles) {
+
+    cryslparserengine = new CrySLParserEngine(CrySLFiles);
+  }
+
+  void SetUp() override { bl::core::get()->set_logging_enabled(false); }
+
+  void TearDown() override { delete cryslparserengine; }
+
+  void compareResults(bool groundTruth, size_t groundValue) {
+
+    size_t numErrors = cryslparserengine.getNumberOfSyntaxErrors();
+    bool typeCheckResult = cryslparserengine.typecheckSucceeded();
+
+    EXPECT_EQ(numErrors, groundValue);
+    EXPECT_EQ(typeCheckResult, groundTruth);
+  }
+
+}; // class Fixture
+
+TEST_F(TypeCheckTest, TypeCheck_Cipher_CTX) {
+  Initialize({pathToCrySLFiles + "EVP_CIPHER_CTX.cryptosl"});
+  bool groundTruth = true;
+  size_t groundValue = 0;
+  compareResults(groundTruth, groundValue);
+}
+
+TEST_F(TypeCheckTest, TypeCheck_Cipher) {
+  Initialize({pathToCrySLFiles + "EVP_CIPHER.cryptosl"});
+  bool groundTruth = true;
+  size_t groundValue = 0;
+  compareResults(groundTruth, groundValue);
+}
+
+TEST_F(TypeCheckTest, TypeCheck_MD_CTX) {
+  Initialize({pathToCrySLFiles + "EVP_MC_CTX.cryptosl"});
+  bool groundTruth = true;
+  size_t groundValue = 0;
+  compareResults(groundTruth, groundValue);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
