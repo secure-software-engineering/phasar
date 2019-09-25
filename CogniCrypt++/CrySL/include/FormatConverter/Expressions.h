f353f073e13284dd2affd3bc41b7017cad7d0e42
shtshpandey1 <shtshpandey1@gmail.com>
2019-09-25 13:19:24 +0200

    addition of subclasses

--- CogniCrypt++/CrySL/include/FormatConverter/Expressions.h
+++ CogniCrypt++/CrySL/include/FormatConverter/Expressions.h
@@ -11,11 +11,61 @@ namespace CCPP {
         virtual ~Expression() = default;
 		virtual std::unique_ptr<Object> evaluate() = 0;
 		virtual Object::TypeT getType() = 0
+		std::vector<Expression> formatconverter(CrySLParser::ConstraintsContext *constraintctx);
 	};
 	class DerefExpression : public Expression
 	{
         virtual std::unique_ptr<Object> evaluate()override;
 		virtual Object::TypeT getType()override;
 	};
+	class AddExpression : public Expression {
+  AddExpression : AddExpression();
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class SubExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class MullExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class DivExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class NotExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class ModExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class EqualExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class NotEqualExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class LessExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class lLessorEqualExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class GreaterorequalExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
+class GreaterExpression : public Expression {
+  virtual std::uni_ptr<object> evaluate() override;
+  virtual Object::TypeT getType() = 0
+};
 
 }
