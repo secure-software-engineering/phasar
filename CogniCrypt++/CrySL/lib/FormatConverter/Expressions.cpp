369205572b90477d09136e3d0c8a5fa31e49f68b
shtshpandey1 <shtshpandey1@gmail.com>
2019-09-25 13:12:11 +0200

    for adeel to get idea

---
+++ CogniCrypt++/CrySL/lib/FormatConverter/Expressions.cpp
@@ -0,0 +1,51 @@
+#include "Expressions.h"
+#include <FormatConverter/Expressions.h>
+#include <FormatConverter/ConstraintsTypeChecker.h>
+#include <iostream>
+
+namespace CCPP {
+//Expression::formatconverter(CrySLTypechecker::typecheck(CrySLParser::ConstraintsContext *constraintctx) {
+	
+  //CrySLTypechecker(ccr->cons()->literalExpr()->memberAccess()->Ident);
+  
+
+	
+  Expression::formatconverter( CrySLParser::ConstraintsContext *constraintctx) {
+
+  
+    
+    for (auto ccr : constraintctx->constr())
+      // CrySLParserParamContext(constraintctx)
+    //temp = typ[0];
+  {
+	  
+    if (ccr->constr.plus) {
+        //if (ccr->constr.size)
+
+        //if (ccr->constr.s)
+        
+        
+      //ccr->cons()->literalExpr()->memberAccess()->Ident();
+      AddExpression addobjct;
+      addobjct.evaluate(constraintctx);
+      addobjct.getType(constraintctx);
+
+	  
+     
+    }
+   
+
+     else if (ccr->comparingRelOperator()) {
+
+    }
+  }
+    Object::TypeT AddExpression::getType(){ if ()
+    
+  }
+  AddExpression::unique_ptr<Object> evaluate() { 
+	  if ()
+  }
+ 
+  }
+  
+ // namespace CCPP
