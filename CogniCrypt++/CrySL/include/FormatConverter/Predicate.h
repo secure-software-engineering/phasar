d3b020467ce885fe0c887a22be16fece4f01e133
Md Sharique <sharique@campus.uni-paderborn.de>
2019-10-06 19:40:51 +0200

    updated library file

--- CogniCrypt++/CrySL/include/FormatConverter/Predicate.h
+++ CogniCrypt++/CrySL/include/FormatConverter/Predicate.h
@@ -3,14 +3,16 @@
 #include <memory>
 #include <phasar/PhasarLLVM>
 #include <FormatConverter/ObjectConverter.h>
-
 #include <string>
+#include <FormatConverter/ObjectWithOutLLVM.h>
+
 
 namespace CCPP {
 class Predicate {
   private:
   bool operator==(const Predicate &pc);
   std::string getFunctionName();
-  std::vector<Object> params;
+  //std::vector<Object> params;
+  std::vector<ObjectWithOutLLVM> params;
 };
 } // namespace CCPP
