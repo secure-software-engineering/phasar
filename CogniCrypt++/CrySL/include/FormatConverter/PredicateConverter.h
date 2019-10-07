565a1148c6dce7f8b2440f114594ed152bfd2271
Md Sharique <sharique@campus.uni-paderborn.de>
2019-10-08 00:09:30 +0200

    Constructor overloaded updated !!!

--- CogniCrypt++/CrySL/include/FormatConverter/PredicateConverter.h
+++ CogniCrypt++/CrySL/include/FormatConverter/PredicateConverter.h
@@ -6,14 +6,14 @@ namespace CCPP {
 	class PredicateConverter {
 	private:
         Predicate predicate;
-		//CrySLParser::EnsPredContext *ensCtx; //not sure, will be used here or not..
+		CrySLParser::EnsPredContext *ensCtx; //not sure, will be used here or not..
         CrySLParser::PredContext *predCtx;
-        //CrySLParser::ReqPredContext *reqCtx; //not sure will be used here or not
+        CrySLParser::ReqPredContext *reqCtx; //not sure will be used here or not
 
 	public:
         PredicateConverter(CrySLParser::PredContext *predicateCtxObj);
-       // PredicateConverter(CrySLParser::EnsPredContext *ensCtxObj);
-       // PredicateConverter(CrySLParser::ReqPredContext *reqCtxObj);
+        PredicateConverter(CrySLParser::EnsPredContext *ensCtxObj);
+		PredicateConverter(CrySLParser::ReqPredContext *reqCtxObj);
         std::vector<Predicate> formatConverter();
         };
 } // namespace CCPP
