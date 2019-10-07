0f77c11d53468fcbd4544717e2c4ea4b32006e00
Md Sharique <sharique@campus.uni-paderborn.de>
2019-10-08 00:44:32 +0200

    update library file and .cpp file work is still left in .cpp file

--- CogniCrypt++/CrySL/include/FormatConverter/Predicate.h
+++ CogniCrypt++/CrySL/include/FormatConverter/Predicate.h
@@ -1,21 +1,26 @@
 #pagma once
-#include <vector>
+#include <FormatConverter/ObjectWithOutLLVM.h>
+#include <Parser/CrySLParser.h>
 #include <set>
 #include <string>
-#include <FormatConverter/ObjectWithOutLLVM.h>
-
+#include <vector>
 
 namespace CCPP {
 class Predicate {
-  private:
-  //std::vector<Object> params;
+private:
   std::vector<ObjectWithOutLLVM> params;
+  std::string functionName;
 
-  public:
+public:
   bool operator == (const Predicate &pc);
-  std::string getFunctionName();
+
+  void setFunctionName(const std::string &functionName) {
+    this->functionName = functionName;
+  }
+  const std::string &getFunctionName() const { return functionName; }
+
   void setParams(const std::vector<ObjectWithOutLLVM> &params) {
-    this->params = params;
+    this->params = params
   }
   const std::vector<ObjectWithOutLLVM> &getparams() const { return params; }
 };
--- CogniCrypt++/CrySL/lib/FormatConverter/Predicate.cpp
+++ CogniCrypt++/CrySL/lib/FormatConverter/Predicate.cpp
@@ -1,10 +1,30 @@
-#include "Predicate.h"
-#include <isotream>
 #include <FormatConverter/Predicate.h>
 #include <FormatConverter/PredicateConverter.h>
+#include <isotream>
 
 namespace CCPP {
-PredicateConverter::PredicateConverter(CrySLParser::PredContext *predicateCtxObj) {
-	this->predCtx = predicateCtxObj;
+PredicateConverter::PredicateConverter(CrySLParser::PredContext *predicateCtxObj) { //TODO
+  this->predCtx = predicateCtxObj;
 }
+
+PredicateConverter::PredicateConverter(CrySLParser::EnsPredContext *ensCtxObj) { //TODO
 }
+
+PredicateConverter::PredicateConverter(CrySLParser::ReqPredContext *reqCtxObj) { //TODO
+}
+
+std::vector<Predicate> PredicateConverter::formatConverter() { //TO CHECK, If needed or not.
+  Predicate predicateObj;
+  std::vector<ObjectWithOutLLVM> params;
+  std::vector<Predicate> predicates;
+
+  /*for (auto prediacte : this->predCtx->Ident()) {
+  }*/
+
+  for (auto ensPred : this->ensCtx->constr()) {
+    predicateObj.getFunctionName();
+	if 
+  }
+}
+bool Predicate::operator==(const Predicate &pc) { return false; } //TODO
+} // namespace CCPP
