cdd452566040848bc65d84ff82f7cccd50c1522f
Md Sharique <sharique@campus.uni-paderborn.de>
2019-10-21 12:04:31 +0200

    changed the object variable from ObjectWithoutLLVM to DefinedObject

--- CogniCrypt++/CrySL/include/FormatConverter/Predicate.h
+++ CogniCrypt++/CrySL/include/FormatConverter/Predicate.h
@@ -17,10 +17,10 @@ public:
   void setFunctionName(const std::string &functionName) {this->functionName = functionName;  }
   const std::string &getFunctionName() const { return functionName; }
 
-  void setParams(const std::vector<ObjectWithOutLLVM> &params) {
+  void setParams(const std::vector<DefinedObject> &params) {
     this->params = params
   }
-  const std::vector<ObjectWithOutLLVM> &getparams() const { return params; }
+  const std::vector<DefinedObject> &getparams() const { return params; }
 };
 
 } // namespace CCPP
