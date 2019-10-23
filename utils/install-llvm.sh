13fe0bfff8cebc1833ec9e74b1d499eb8d3385ac
Philipp Schubert <philipp@it-schubert.com>
2019-10-23 09:45:48 +0200

    minor adjustments

--- utils/install-llvm.sh
+++ utils/install-llvm.sh
@@ -5,14 +5,16 @@ set -e
 num_cores=1
 target_dir=./
 re_number="^[0-9]+$"
+re_llvm_release="^llvmorg-[0-9]+\.[0-9]+\.[0-9]+$"
 
-if [ "$#" -ne 2 ] || ! [[ "$1" =~ ${re_number} ]] || ! [ -d "$2" ]; then
-	echo "usage: <prog> <# cores> <directory>" >&2
+if [ "$#" -ne 3 ] || ! [[ "$1" =~ ${re_number} ]] || ! [ -d "$2" ] || ! [[ "$3" =~ ${re_llvm_release} ]]; then
+	echo "usage: <prog> <# cores> <directory> <LLVM release (e.g. 'llvmorg-9.0.0')>" >&2
 	exit 1
 fi
 
 num_cores=$1
 target_dir=$2
+llvm_release=$3
 
 echo "Getting the LLVM source code..."
 if [ ! -d "${target_dir}/llvm-project" ]; then
@@ -20,7 +22,7 @@ if [ ! -d "${target_dir}/llvm-project" ]; then
 fi
 echo "Building LLVM..."
 cd ${target_dir}/llvm-project/
-git checkout llvmorg-8.0.0
+git checkout ${llvm_release}
 mkdir -p build
 cd build
 cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;libcxx;libcxxabi;libunwind;lld;lldb;compiler-rt;lld;polly;debuginfo-tests;openmp;parallel-libs' -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_CXX1Y=ON -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DBUILD_SHARED_LIBS=ON -DLLVM_BUILD_LLVM_DYLIB=ON ../llvm
