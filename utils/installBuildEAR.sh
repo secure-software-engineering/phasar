e18a89af91cadb999edc9007f007c78f60f8c97e
Joseph <28813094+J03D03@users.noreply.github.com>
2020-04-07 15:33:43 +0200

    update installBuildEAR.sh script

--- utils/installBuildEAR.sh
+++ utils/installBuildEAR.sh
@@ -4,8 +4,9 @@ set -e
 echo "Getting the complete Build EAR source code"
 tmp_dir=`mktemp -d "Bear.XXXXXXXX" --tmpdir`
 git clone https://github.com/rizsotto/Bear.git ${tmp_dir}
-cd ${tmp_dir}
-cmake .
+mkdir -p ${tmp_dir}/build
+cd ${tmp_dir}/build
+cmake ..
 make install
 make check
 make package
