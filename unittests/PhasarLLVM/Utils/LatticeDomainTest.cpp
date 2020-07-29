15a4cd6df00b7d6bb7fe919cef193b64a5632a46
Florian Sattler <vuld3r@gmail.com>
2020-07-29 13:05:04 +0200

    Fixes LatticeDomain lesser comparison

---
+++ unittests/PhasarLLVM/Utils/LatticeDomainTest.cpp
@@ -0,0 +1,64 @@
+#include "gtest/gtest.h"
+
+#include "phasar/PhasarLLVM/Utils/LatticeDomain.h"
+
+using namespace psr;
+
+TEST(LatticeDomain, topShouldNotBeLessThanTop) {
+  LatticeDomain<int> LD1(Top{});
+  LatticeDomain<int> LD2(Top{});
+
+  EXPECT_FALSE(LD1 < LD2);
+  EXPECT_FALSE(LD2 < LD1);
+}
+
+TEST(LatticeDomain, bottomShouldNotBeLessThanBottom) {
+  LatticeDomain<int> LD1(Bottom{});
+  LatticeDomain<int> LD2(Bottom{});
+
+  EXPECT_FALSE(LD1 < LD2);
+  EXPECT_FALSE(LD2 < LD1);
+}
+
+TEST(LatticeDomain, topShouldBeLessThanBottom) {
+  LatticeDomain<int> LD1(Top{});
+  LatticeDomain<int> LD2(Bottom{});
+
+  EXPECT_TRUE(LD1 < LD2);
+}
+
+TEST(LatticeDomain, bottomShouldNotBeLessThanTop) {
+  LatticeDomain<int> LD1(Top{});
+  LatticeDomain<int> LD2(Bottom{});
+
+  EXPECT_FALSE(LD2 < LD1);
+}
+
+TEST(LatticeDomain, lessShouldCorrectlyForwardToInnerLatticeType) {
+  LatticeDomain<int> LD1(42);
+  LatticeDomain<int> LD2(21);
+
+  EXPECT_FALSE(LD1 < LD2);
+  EXPECT_TRUE(LD2 < LD1);
+}
+
+TEST(LatticeDomain, innerLatticeTypeShouldBeNotLessThanTop) {
+  LatticeDomain<int> LD1(42);
+  LatticeDomain<int> LD2(Top{});
+
+  EXPECT_FALSE(LD1 < LD2);
+  EXPECT_TRUE(LD2 < LD1);
+}
+
+TEST(LatticeDomain, innerLatticeTypeShouldBeLessThanBottom) {
+  LatticeDomain<int> LD1(42);
+  LatticeDomain<int> LD2(Bottom{});
+
+  EXPECT_TRUE(LD1 < LD2);
+  EXPECT_FALSE(LD2 < LD1);
+}
+
+int main(int Argc, char **Argv) {
+  ::testing::InitGoogleTest(&Argc, Argv);
+  return RUN_ALL_TESTS();
+}
