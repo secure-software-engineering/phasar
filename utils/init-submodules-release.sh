b40d9531f36b7d11b5fce1f50fb6cad71c017686
hexa <61878361+vaibhav-hexa@users.noreply.github.com>
2021-01-12 13:15:40 +0100

    Update utils/init-submodules-release.sh
    Co-authored-by: Florian Sattler <vuld3r@gmail.com>

--- utils/init-submodules-release.sh
+++ utils/init-submodules-release.sh
@@ -5,7 +5,7 @@ save_cd "$(dirname "$0")"/../external/
 git clone git@github.com:google/googletest.git
 cd googletest/ || exit
 git checkout release-1.8.0
-cd - || exit
+save_cd -
 git clone git@github.com:nlohmann/json.git
 cd json/ || exit
 git checkout v3.4.0
