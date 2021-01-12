739c8f8e9211ac498cac9b6edf92ab7ddf761578
hexa <61878361+vaibhav-hexa@users.noreply.github.com>
2021-01-12 13:16:02 +0100

    Update utils/init-submodules-release.sh
    Co-authored-by: Florian Sattler <vuld3r@gmail.com>

--- utils/init-submodules-release.sh
+++ utils/init-submodules-release.sh
@@ -3,7 +3,7 @@
 if git submodule status 2>&1 | grep -iq "fatal: Not a git repository (or any of the parent directories): .git"; then
 save_cd "$(dirname "$0")"/../external/
 git clone git@github.com:google/googletest.git
-cd googletest/ || exit
+save_cd googletest/
 git checkout release-1.8.0
 save_cd -
 git clone git@github.com:nlohmann/json.git
