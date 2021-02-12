11805c7866ed6d099c33c4eaff245fde5fbaeb2f
Alexander Meinhold <35108195+alexmeinhold@users.noreply.github.com>
2021-02-12 09:15:17 +0100

    Exception Handling Testing (#231)
    * add ir file for exception handling testing
    
    * add exception example to cmake
    
    * move exception example to dummy_source_sink/
    
    * add simple taint analysis test with exception handling
    
    * add more exception handling tests
    
    * add more llvm test code for exception handling
    
    * add mem2reg test as well
    
    * add try/catch example for exception handling testing
    
    * fix unittest
    
    * add more exception handling example files and test cases
    
    * clean up
    
    * add nested try/catch example for exception handling testing
    
    * test multiple try/catch blocks
    
    * remove redundant test cases
    
    * run clang format

---
+++ test/llvm_test_code/taint_analysis/dummy_source_sink/taint_exception_08.cpp
@@ -0,0 +1,21 @@
+extern int source();
+extern void sink(int data);
+
+struct S {
+  int data;
+  S(int data) : data(data) {}
+};
+
+int main() {
+  int data;
+  try {
+    S *s = new S(0);
+    try {
+      data = source();
+    } catch (...) {
+    }
+  } catch (...) {
+  }
+  sink(data);
+  return 0;
+}
