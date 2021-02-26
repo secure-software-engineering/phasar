db732e8343e31f89db014ce9409b36ea6118f379
linuxfan91 <linuxfan91@googlemail.com>
2021-02-26 12:52:36 +0100

    Fix BitVectorSet

--- include/phasar/Utils/BitVectorSet.h
+++ include/phasar/Utils/BitVectorSet.h
@@ -265,7 +265,10 @@ public:
     }
   }
 
-  void clear() noexcept { Bits.reset(); }
+  void clear() noexcept {
+    Bits.clear();
+    Bits.resize(0);
+  }
 
   [[nodiscard]] bool empty() const noexcept { return Bits.none(); }
 
@@ -286,33 +289,7 @@ public:
   [[nodiscard]] size_t size() const noexcept { return Bits.count(); }
 
   friend bool operator==(const BitVectorSet &Lhs, const BitVectorSet &Rhs) {
-    // return Lhs.Bits == Rhs.Bits;
-
-    typedef uintptr_t BitWord;
-    enum { BITWORD_SIZE = (unsigned)sizeof(BitWord) * CHAR_BIT };
-    static_assert(BITWORD_SIZE == 64 || BITWORD_SIZE == 32,
-                  "Unsupported word size");
-    unsigned lhssize = Lhs.size();
-    unsigned rhssize = Rhs.size();
-    unsigned LHSWords = (lhssize + BITWORD_SIZE - 1) / BITWORD_SIZE;
-    unsigned RHSWords = (rhssize + BITWORD_SIZE - 1) / BITWORD_SIZE;
-    unsigned i;
-    for (i = 0; i != std::min(LHSWords, RHSWords); ++i) {
-      if (Lhs.Bits[i] != Rhs.Bits[i])
-        return false;
-    }
-    // Verify that any extra words are all zeros.
-    if (i != LHSWords) {
-      for (; i != LHSWords; ++i)
-        if (Lhs.Bits[i])
-          return false;
-    } else if (i != RHSWords) {
-      for (; i != RHSWords; ++i)
-        if (Rhs.Bits[i])
-          return false;
-    }
-
-    return true;
+    return Lhs.Bits == Rhs.Bits;
   }
 
   friend bool operator!=(const BitVectorSet &Lhs, const BitVectorSet &Rhs) {
