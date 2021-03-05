c1a49a539d2bd2394e6907cc26da81e05b531f89
Philipp Schubert <philipp@it-schubert.com>
2021-03-05 13:26:08 +0100

    clang-format

--- include/phasar/PhasarLLVM/DataFlowSolver/IfdsIde/FlowFactWrapper.h
+++ include/phasar/PhasarLLVM/DataFlowSolver/IfdsIde/FlowFactWrapper.h
@@ -104,7 +104,7 @@ public:
   }
 
   template <typename... Args>
-  std::set<const FlowFact *> getOrCreateFlowFacts(Args &&...args) {
+  std::set<const FlowFact *> getOrCreateFlowFacts(Args &&... args) {
     std::set<const FlowFact *> Ret;
     (Ret.insert(getOrCreateFlowFact(std::forward<Args>(args))), ...);
     return Ret;
