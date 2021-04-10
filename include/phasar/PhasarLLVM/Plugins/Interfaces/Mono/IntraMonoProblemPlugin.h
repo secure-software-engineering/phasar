36f03ab206f552212a1207db07911ea65fe32de4
Martin Ross <mross_public@pobox.com>
2021-04-09 21:06:25 -0400

    Add explicit cast to llvm::Value*

--- include/phasar/PhasarLLVM/Plugins/Interfaces/Mono/IntraMonoProblemPlugin.h
+++ include/phasar/PhasarLLVM/Plugins/Interfaces/Mono/IntraMonoProblemPlugin.h
@@ -33,7 +33,7 @@ public:
       : IntraMonoProblem(IRDB, TH, CF, PT, EntryPoints) {}
 
   void printNode(std::ostream &os, n_t n) const override {
-    os << llvmIRToString(n);
+    os << llvmIRToString((llvm::Value *) n);
   }
   void printDataFlowFact(std::ostream &os, d_t d) const override {
     os << llvmIRToString(d);
