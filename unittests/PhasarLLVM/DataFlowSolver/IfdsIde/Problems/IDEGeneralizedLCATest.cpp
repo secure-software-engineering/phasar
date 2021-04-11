224747b6c39500fa9b104bb8737112db69fc49fe
Martin Ross <mross_public@pobox.com>
2021-04-11 15:51:09 -0400

    Update IDEGeneralizedLCATest.cpp

--- unittests/PhasarLLVM/DataFlowSolver/IfdsIde/Problems/IDEGeneralizedLCATest.cpp
+++ unittests/PhasarLLVM/DataFlowSolver/IfdsIde/Problems/IDEGeneralizedLCATest.cpp
@@ -158,7 +158,7 @@ TEST_F(IDEGeneralizedLCATest, Imprecision) {
   Initialize("Imprecision_c.ll", 2);
 //   auto xInst = IRDB->getInstruction(0); // foo.x
 //   auto yInst = IRDB->getInstruction(1); // foo.y
-  auto barInst = IRDB->getInstruction(7);
+//  auto barInst = IRDB->getInstruction(7);
 
   // std::cout << "foo.x = " << LCASolver->resultAt(barInst, xInst) <<
   // std::endl; std::cout << "foo.y = " << LCASolver->resultAt(barInst, yInst)
