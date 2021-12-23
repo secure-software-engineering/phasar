81032a4001e059fa960af8a3c11896dbc5fcc368
Martin Mory <linuxfan91@googlemail.com>
2021-12-23 14:16:20 +0100

    clang-tidy fixes WPDS solver and problems - mostly renames

--- include/phasar/PhasarLLVM/DataFlowSolver/WPDS/WPDSSolverConfig.h
+++ include/phasar/PhasarLLVM/DataFlowSolver/WPDS/WPDSSolverConfig.h
@@ -18,17 +18,17 @@ namespace psr {
 
 struct WPDSSolverConfig {
   WPDSSolverConfig() = default;
-  WPDSSolverConfig(bool recordWitnesses, WPDSSearchDirection searchDirection,
-                   WPDSType wpdsty);
+  WPDSSolverConfig(bool RecordWitnesses, WPDSSearchDirection Direction,
+                   WPDSType WPDSTy);
   ~WPDSSolverConfig() = default;
   WPDSSolverConfig(const WPDSSolverConfig &) = default;
   WPDSSolverConfig &operator=(const WPDSSolverConfig &) = default;
   WPDSSolverConfig(WPDSSolverConfig &&) = default;
   WPDSSolverConfig &operator=(WPDSSolverConfig &&) = default;
-  bool recordWitnesses = false;
-  WPDSSearchDirection searchDirection = WPDSSearchDirection::FORWARD;
-  WPDSType wpdsty = WPDSType::FWPDS;
-  friend std::ostream &operator<<(std::ostream &os, const WPDSSolverConfig &sc);
+  bool RecordWitnesses = false;
+  WPDSSearchDirection Direction = WPDSSearchDirection::FORWARD;
+  WPDSType WPDSType = WPDSType::FWPDS;
+  friend std::ostream &operator<<(std::ostream &OS, const WPDSSolverConfig &SC);
 };
 
 } // namespace psr
