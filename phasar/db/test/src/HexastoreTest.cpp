e3e15e125e3bbc53f0f15fb775e61526eb7dd8fe
Lucas Briese <lucas.briese@iem.fraunhofer.de>
2022-07-27 15:21:34 +0200

    restructure: phasar-db-test working

--- phasar/db/test/src/HexastoreTest.cpp
+++ phasar/db/test/src/HexastoreTest.cpp
@@ -268,8 +268,3 @@ TEST(HexastoreTest, StoreGraphWithEdgeLabels) {
   // }
   ASSERT_TRUE(boost::isomorphism(I, J));
 }
-
-int main(int Argc, char **Argv) {
-  ::testing::InitGoogleTest(&Argc, Argv);
-  return RUN_ALL_TESTS();
-}
