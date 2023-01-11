b4e911ce3d5e0043a813efbeffcf66c6c2501df2
Fabian Schiebel <fabian.schiebel@iem.fraunhofer.de>
2023-01-11 10:17:56 +0100

    Make install optional in bootstrap.sh + adapt readme

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -e
+
+set -eo pipefail
 
 source ./utils/safeCommandsSet.sh
 
@@ -10,6 +11,8 @@ readonly LLVM_INSTALL_DIR="/usr/local/llvm-14"
 NUM_THREADS=$(nproc)
 LLVM_RELEASE=llvmorg-14.0.0
 DO_UNIT_TEST=true
+DO_INSTALL=false
+BUILD_TYPE=Release
 
 
 # Parsing command-line-parameters
@@ -48,6 +51,14 @@ case $key in
     DESIRED_BOOST_VERSION="${key#*=}"
     shift # past argument=value
     ;;
+    -DCMAKE_BUILD_TYPE=*)
+    BUILD_TYPE="${key#*=}"
+    shift # past argument=value
+    ;;
+    --install)
+    DO_INSTALL=true
+    shift # past argument
+    ;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
     shift # past argument
@@ -60,12 +71,13 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 echo "installing phasar dependencies..."
 if [ -x "$(command -v pacman)" ]; then
-    yes | sudo pacman -Syu --needed which zlib sqlite3 ncurses make python3 doxygen libxml2 swig gcc cmake z3 libedit graphviz python-sphinx openmp curl python-pip
+    yes | sudo pacman -Syu --needed which zlib sqlite3 ncurses make python3 doxygen libxml2 swig gcc z3 libedit graphviz python-sphinx openmp curl python-pip
     ./utils/installBuildEAR.sh
 else
     ./utils/InstallAptDependencies.sh
 fi
-sudo pip3 install Pygments pyyaml
+
+pip3 install cmake
 
 if [ ! -z "${DESIRED_BOOST_DIR}" ]; then
     BOOST_PARAMS="-DBOOST_ROOT=${DESIRED_BOOST_DIR}"
@@ -95,65 +107,80 @@ else
         fi
 	else
         echo "Already installed boost version ${BOOST_VERSION//_/.}"
-        if [ -x "$(command -v apt)" ]; then
-            DESIRED_BOOST_VERSION=${BOOST_VERSION//_/.}
-            # install missing packages if necessary
-            boostlibnames=("libboost-system" "libboost-filesystem"
-                    "libboost-graph" "libboost-program-options"
-                    "libboost-thread")
-            additional_boost_libs=()
-            for boost_lib in ${boostlibnames[@]}; do
-                dpkg -s "$boost_lib${DESIRED_BOOST_VERSION}" >/dev/null 2>&1 ||
-                dpkg -s "$boost_lib${DESIRED_BOOST_VERSION}.0" >/dev/null 2>&1 ||
-                additional_boost_libs+=("$boost_lib${DESIRED_BOOST_VERSION}") ||
-                additional_boost_libs+=("$boost_lib${DESIRED_BOOST_VERSION}.0")
-                dpkg -s "${boost_lib}-dev" >/dev/null 2>&1 || additional_boost_libs+=("${boost_lib}-dev")
-            done
-            if [ ${#additional_boost_libs[@]} -gt 0 ] ;then
-                echo "Installing additional ${#additional_boost_libs[@]} boost packages: ${additional_boost_libs[*]}"
-                sudo apt install "${additional_boost_libs[@]}" -y
-            fi
-        fi
+        # if [ -x "$(command -v apt)" ]; then
+        #     DESIRED_BOOST_VERSION=${BOOST_VERSION//_/.}
+        #     # install missing packages if necessary
+        #     boostlibnames=("libboost-system" "libboost-filesystem"
+        #             "libboost-graph" "libboost-program-options"
+        #             "libboost-thread")
+        #     additional_boost_libs=()
+        #     for boost_lib in ${boostlibnames[@]}; do
+        #         dpkg -s "$boost_lib${DESIRED_BOOST_VERSION}" >/dev/null 2>&1 ||
+        #         dpkg -s "$boost_lib${DESIRED_BOOST_VERSION}.0" >/dev/null 2>&1 ||
+        #         additional_boost_libs+=("$boost_lib${DESIRED_BOOST_VERSION}") ||
+        #         additional_boost_libs+=("$boost_lib${DESIRED_BOOST_VERSION}.0")
+        #         dpkg -s "${boost_lib}-dev" >/dev/null 2>&1 || additional_boost_libs+=("${boost_lib}-dev")
+        #     done
+        #     if [ ${#additional_boost_libs[@]} -gt 0 ] ;then
+        #         echo "Installing additional ${#additional_boost_libs[@]} boost packages: ${additional_boost_libs[*]}"
+        #         sudo apt install "${additional_boost_libs[@]}" -y
+        #     fi
+        # fi
 	fi
 fi
 
 # installing LLVM
-tmp_dir=$(mktemp -d "llvm-10_build.XXXXXXXX" --tmpdir)
+tmp_dir=$(mktemp -d "llvm-build.XXXXXXXX" --tmpdir)
 ./utils/install-llvm.sh "${NUM_THREADS}" "${tmp_dir}" ${LLVM_INSTALL_DIR} ${LLVM_RELEASE}
 rm -rf "${tmp_dir}"
-sudo pip3 install wllvm
 echo "dependencies successfully installed"
 
 
 echo "Building PhASAR..."
 ${DO_UNIT_TEST} && echo "with unit tests."
+
+echo "Updating the submodules..."
 git submodule init
 git submodule update
+echo "Submodules successfully updated"
+
 
 export CC=${LLVM_INSTALL_DIR}/bin/clang
 export CXX=${LLVM_INSTALL_DIR}/bin/clang++
 
 mkdir -p "${PHASAR_DIR}"/build
 safe_cd "${PHASAR_DIR}"/build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release "${BOOST_PARAMS}" -DPHASAR_BUILD_UNITTESTS=${DO_UNIT_TEST} "${PHASAR_DIR}"
-cmake --build .
+cmake -G Ninja -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" "${BOOST_PARAMS}" -DPHASAR_BUILD_UNITTESTS="${DO_UNIT_TEST}" "${PHASAR_DIR}"
+cmake --build . -j "${NUM_THREADS}"
+
+echo "phasar successfully built"
 
 if ${DO_UNIT_TEST}; then
    echo "Running PhASAR unit tests..."
+
+   NUM_FAILED_TESTS=0
+
    pushd unittests
    for x in $(find . -type f -executable -print); do
-       pushd "${x%/*}" && ./"${x##*/}" && popd || { echo "Test ${x} failed, aborting."; exit 1; };
+       pushd "${x%/*}" && ./"${x##*/}" || { echo "Test ${x} failed."; NUM_FAILED_TESTS=$((NUM_FAILED_TESTS+1)); };
+       popd;
        done
    popd
+
+   echo "Finished running PhASAR unittests"
+   echo "${NUM_FAILED_TESTS} tests failed"
 fi
 
-echo "phasar successfully built"
-echo "install phasar..."
-sudo cmake -DCMAKE_INSTALL_PREFIX=${PHASAR_INSTALL_DIR} -P cmake_install.cmake
-sudo ldconfig
-safe_cd ..
-echo "phasar successfully installed to ${PHASAR_INSTALL_DIR}"
 
+if ${DO_INSTALL}; then
+    echo "install phasar..."
+    sudo cmake -DCMAKE_INSTALL_PREFIX=${PHASAR_INSTALL_DIR} -P cmake_install.cmake
+    sudo ldconfig
+    safe_cd ..
+    echo "phasar successfully installed to ${PHASAR_INSTALL_DIR}"
 
-echo "Set environment variables"
-./utils/setEnvironmentVariables.sh ${LLVM_INSTALL_DIR} ${PHASAR_INSTALL_DIR}
+    echo "Set environment variables"
+    ./utils/setEnvironmentVariables.sh ${LLVM_INSTALL_DIR} ${PHASAR_INSTALL_DIR}
+fi
+
+echo "done."
