64cfbf9debdb12266f96e2450e2a601ab89b87da
Fabian Schiebel <52407375+fabianbs96@users.noreply.github.com>
2023-01-17 20:14:11 +0100

    Bootstrap Update (#559)
    * Make install optional in bootstrap.sh + adapt readme
    
    * minor
    
    * Re-enable installing additional boost packages if boost is already installed
    
    * Update bootstrap.sh as per review comment
    
    Co-authored-by: Florian Sattler <vuld3r@gmail.com>
    
    Co-authored-by: Florian Sattler <vuld3r@gmail.com>
    Co-authored-by: Martin Mory <mmo@mail.upb.de>

--- utils/InstallAptDependencies.sh
+++ utils/InstallAptDependencies.sh
@@ -2,5 +2,5 @@
 set -e
 
 sudo apt update
-sudo apt install git make cmake -y
-sudo apt install zlib1g-dev sqlite3 libsqlite3-dev bear python3 doxygen graphviz python3-pip libxml2 libxml2-dev libncurses5-dev libncursesw5-dev swig build-essential g++ cmake libz3-dev libedit-dev python3-sphinx libomp-dev libcurl4-openssl-dev ninja-build -y
+sudo apt install git -y
+sudo apt install zlib1g-dev sqlite3 libsqlite3-dev bear python3 doxygen graphviz python3-pip libxml2 libxml2-dev libncurses5-dev libncursesw5-dev swig build-essential g++ libz3-dev libedit-dev python3-sphinx libomp-dev libcurl4-openssl-dev ninja-build -y
