635dee6a27d7fe71c2819adcb1d99f1dbc0a6cf6
Lucas Briese <lucas.briese@iem.fraunhofer.de>
2023-02-14 18:23:30 +0100

    fix: deploy / previous merge

--- utils/conan/deploy_phasar.sh
+++ utils/conan/deploy_phasar.sh
@@ -14,8 +14,8 @@ set -euo pipefail
     package="phasar/$calver+$short_hash@"
     
     # from conanfile.txt
-    options=(-o llvm:shared=False -o llvm:enable_debug=True -o llvm:with_project_clang=True -o llvm:with_project_openmp=True -o llvm:keep_binaries_regex="^(clang|clang\+\+|opt)$")
-    cmd=(conan create "$(pwd)/phasar/" "$package" "${options[@]}")
+    options=(-o llvm:shared=False -o llvm:enable_debug=True -o llvm:with_project_clang=True -o llvm:with_project_openmp=True -o llvm:with_runtime_compiler-rt=True -o llvm:keep_binaries_regex="^(clang|clang\+\+|opt)$")
+    cmd=(conan create "$(pwd)/phasar/" "$package" "${options[@]}" --build=missing)
     if [ "$#" -gt 0 ]; then
         user_options+=("$@")
     fi
