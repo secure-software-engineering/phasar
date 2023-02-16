123e50258e3d733e01985b81491b1829efd1eba5
Fabian Schiebel <52407375+fabianbs96@users.noreply.github.com>
2023-02-16 19:01:14 +0100

    Fix include-guard according to review

--- include/phasar/PhasarLLVM/TaintConfig/TaintConfigUtilities.h
+++ include/phasar/PhasarLLVM/TaintConfig/TaintConfigUtilities.h
@@ -7,8 +7,8 @@
  *     Philipp Schubert and others
  *****************************************************************************/
 
-#ifndef PHASAR_PHASARLLVM_LLVMTaintConfig_LLVMTaintConfigUTILITIES_H
-#define PHASAR_PHASARLLVM_LLVMTaintConfig_LLVMTaintConfigUTILITIES_H
+#ifndef PHASAR_PHASARLLVM_TAINTCONFIG_LLVMTAINTCONFIGUTILITIES_H
+#define PHASAR_PHASARLLVM_TAINTCONFIG_LLVMTAINTCONFIGUTILITIES_H
 
 #include "phasar/PhasarLLVM/TaintConfig/LLVMTaintConfig.h"
 #include "phasar/PhasarLLVM/Utils/LLVMShorthands.h"
@@ -87,4 +87,4 @@ void collectSanitizedFacts(ContainerTy &Dest, const LLVMTaintConfig &Config,
 }
 } // namespace psr
 
-#endif
+#endif // PHASAR_PHASARLLVM_TAINTCONFIG_LLVMTAINTCONFIGUTILITIES_H
