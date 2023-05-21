6ccc6ff7b42c5508942b177a8d155e928136c89f
Fabian Schiebel <fabian.schiebel@iem.fraunhofer.de>
2023-05-21 20:35:25 +0200

    Add consolidated header files

---
+++ include/phasar/PhasarPass.h
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * Copyright (c) 2023 Philipp Schubert.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of LICENSE.txt.
+ *
+ * Contributors:
+ *     Fabian Schiebel and others
+ *****************************************************************************/
+
+#ifndef PHASAR_PHASARPASS_H
+#define PHASAR_PHASARPASS_H
+
+#include "PhasarPass/Options.h"
+#include "PhasarPass/PhasarPass.h"
+#include "PhasarPass/PhasarPrinterPass.h"
+#include "PhasarPass/RegisterPasses.h"
+
+#endif // PHASAR_PHASARPASS_H
