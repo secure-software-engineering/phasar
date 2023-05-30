9428cb54a52e6da3e64b066bf07728803ef20227
mxHuber <huber.maximilian.leo@gmail.com>
2023-05-30 12:52:40 +0200

    backup save, still needs metadata extraction

--- include/phasar/PhasarLLVM/TypeHierarchy/LLVMTypeHierarchy_MD.h
+++ include/phasar/PhasarLLVM/TypeHierarchy/LLVMTypeHierarchy_MD.h
@@ -15,6 +15,7 @@
 #include <optional>
 #include <set>
 #include <string>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -22,8 +23,23 @@
 #include <llvm-14/llvm/IR/DerivedTypes.h>
 #include <llvm-14/llvm/IR/Module.h>
 
+namespace llvm {
+class Module;
+class StructType;
+class Function;
+class GlobalVariable;
+} // namespace llvm
+
 namespace psr {
 
+class LLVMProjectIRDB;
+/**
+ * 	@brief Owns the class hierarchy of the analyzed program.
+ *
+ * 	This class is responsible for constructing a inter-modular class
+ * 	hierarchy graph based on the data from the %ProjectIRCompiledDB
+ * 	and reconstructing the virtual method tables.
+ */
 class LLVMTypeHierarchy_MD
     : public TypeHierarchy<const llvm::StructType *, const llvm::Function *> {
 public:
@@ -38,29 +54,103 @@ public:
     std::set<const llvm::StructType *> ReachableTypes;
   };
 
-  /// Edges in the class hierarchy graph doesn't hold any additional
-  /// information.
-  struct EdgeProperties {
-    EdgeProperties() = default;
+  struct Graph {
+    Graph() = default;
+
+    std::vector<VertexProperties> Vertices;
+    // Adjacencies between vertices are stored here by their index in the
+    // 'Vertices' vector
+    std::vector<std::tuple<int>> Adjacencies;
   };
+
+  /**
+   *  @brief Creates a LLVMStructTypeHierarchy based on the
+   *         given ProjectIRCompiledDB.
+   *  @param IRDB ProjectIRCompiledDB object.
+   */
+  LLVMTypeHierarchy_MD(LLVMProjectIRDB &IRDB);
+
+  /**
+   *  @brief Creates a LLVMStructTypeHierarchy based on the
+   *         llvm::Module.
+   *  @param M A llvm::Module.
+   */
+  LLVMTypeHierarchy_MD(const llvm::Module &M);
+  ~LLVMTypeHierarchy_MD() override = default;
 
   /**
    * @brief Constructs the actual class hierarchy graph.
    * @param M LLVM module
    *
    * Extracts new information from the given module and adds new vertices
-   * and edges accordingly to the type hierarchy graph.
+   * accordingly to the type hierarchy graph.
    */
   void constructHierarchy(const llvm::Module &M);
 
-  /// Data structure holding the class hierarchy graph.
-  using bidigraph_t =
-      boost::adjacency_list<boost::setS, boost::vecS, boost::bidirectionalS,
-                            VertexProperties, EdgeProperties>;
+  [[nodiscard]] inline bool
+  hasType(const llvm::StructType *Type) const override;
+
+  [[nodiscard]] inline bool isSubType(const llvm::StructType *Type,
+                                      const llvm::StructType *SubType) override;
+
+  std::set<const llvm::StructType *>
+  getSubTypes(const llvm::StructType *Type) override;
+
+  [[nodiscard]] inline bool
+  isSuperType(const llvm::StructType *Type,
+              const llvm::StructType *SuperType) override;
+
+  std::set<const llvm::StructType *>
+  getSuperTypes(const llvm::StructType *Type) override;
+
+  [[nodiscard]] const llvm::StructType *
+  getType(std::string TypeName) const override;
+
+  [[nodiscard]] std::set<const llvm::StructType *> getAllTypes() const override;
+
+  [[nodiscard]] std::string
+  getTypeName(const llvm::StructType *Type) const override;
+
+  [[nodiscard]] bool hasVFTable(const llvm::StructType *Type) const override;
+
+  [[nodiscard]] const LLVMVFTable *
+  getVFTable(const llvm::StructType *Type) const override;
+
+  [[nodiscard]] inline size_t size() const override;
+
+  [[nodiscard]] inline bool empty() const override;
+
+  void print(llvm::raw_ostream &OS = llvm::outs()) const override;
+
+  [[nodiscard]] nlohmann::json getAsJson() const override;
+
+  static inline constexpr llvm::StringLiteral StructPrefix = "struct.";
+  static inline constexpr llvm::StringLiteral ClassPrefix = "class.";
+  static inline constexpr llvm::StringLiteral VTablePrefix = "_ZTV";
+  static inline constexpr llvm::StringLiteral VTablePrefixDemang =
+      "vtable for ";
+  static inline constexpr llvm::StringLiteral TypeInfoPrefix = "_ZTI";
+  static inline constexpr llvm::StringLiteral TypeInfoPrefixDemang =
+      "typeinfo for ";
+  static inline constexpr llvm::StringLiteral PureVirtualCallName =
+      "__cxa_pure_virtual";
+
+protected:
+  void buildLLVMTypeHierarchy(const llvm::Module &M);
 
 private:
-  bidigraph_t TypeGraph;
+  Graph TypeGraph;
+  // holds all modules that are included in the type hierarchy
+  std::unordered_set<const llvm::Module *> VisitedModules;
+  // holds all metadata notes
   std::vector<llvm::MDNode *> MetadataNotes;
+  std::unordered_map<const llvm::StructType *, LLVMVFTable> TypeVFTMap;
+  // helper map from clearname to type*
+  std::unordered_map<std::string, const llvm::StructType *> ClearNameTypeMap;
+
+  static std::string removeStructOrClassPrefix(const llvm::StructType &T);
+
+  static std::string removeStructOrClassPrefix(const std::string &TypeName);
 
   /**
    * 	@brief Prints the class hierarchy to an ostream in dot format.
--- lib/PhasarLLVM/TypeHierarchy/LLVMTypeHierarchy_MD.cpp
+++ lib/PhasarLLVM/TypeHierarchy/LLVMTypeHierarchy_MD.cpp
@@ -1,23 +1,11 @@
-
 #include "phasar/PhasarLLVM/TypeHierarchy/LLVMTypeHierarchy_MD.h"
 
-#include <llvm-14/llvm/IR/Metadata.h>
+#include "phasar/PhasarLLVM/DB/LLVMProjectIRDB.h"
+#include "phasar/Utils/Logger.h"
+
+#include "llvm/IR/Metadata.h"
 
 namespace psr {
-
-// provide a VertexPropertyWrite to tell boost how to write a vertex
-class TypeHierarchyVertexWriter {
-public:
-  TypeHierarchyVertexWriter(const LLVMTypeHierarchy_MD::bidigraph_t &TyGraph)
-      : TyGraph(TyGraph) {}
-  template <class VertexOrEdge>
-  void operator()(std::ostream &Out, const VertexOrEdge &V) const {
-    Out << "[label=\"" << TyGraph[V].getTypeName() << "\"]";
-  }
-
-private:
-  const LLVMTypeHierarchy_MD::bidigraph_t &TyGraph;
-};
 
 LLVMTypeHierarchy_MD::VertexProperties::VertexProperties(
     const llvm::StructType *Type)
@@ -27,16 +15,108 @@ std::string LLVMTypeHierarchy_MD::VertexProperties::getTypeName() const {
   return Type->getStructName().str();
 }
 
-void constructHierarchy(const llvm::Module &M) {
-  // TODO:
-  /*
-  for (unsigned int i = 0; i < Node->getNumOperands(); i++) {
-      const llvm::MDOperand &CurrentOp = Node->getOperand(i);
-  }
+LLVMTypeHierarchy_MD::LLVMTypeHierarchy_MD(LLVMProjectIRDB &IRDB) {
+  PHASAR_LOG_LEVEL(INFO, "Construct type hierarchy");
 
-  // What's that? Could be useful. Look up later
-  // current_MD.second->printTree();
-  */
+  // TODO (max): getMetadata() here
+
+  buildLLVMTypeHierarchy(*IRDB.getModule());
+}
+
+LLVMTypeHierarchy_MD::LLVMTypeHierarchy_MD(const llvm::Module &M) {
+  PHASAR_LOG_LEVEL_CAT(INFO, "LLVMTypeHierarchy", "Construct type hierarchy");
+  buildLLVMTypeHierarchy(M);
+  PHASAR_LOG_LEVEL_CAT(INFO, "LLVMTypeHierarchy", "Finished type hierarchy");
+}
+
+void LLVMTypeHierarchy_MD::buildLLVMTypeHierarchy(const llvm::Module &M) {
+  // build the hierarchy for the module
+  constructHierarchy(M);
+  // cache the reachable types
+
+  // TODO (max): implement the caching of reachable types
+}
+
+void LLVMTypeHierarchy_MD::constructHierarchy(const llvm::Module &M) {
+  PHASAR_LOG_LEVEL_CAT(DEBUG, "LLVMTypeHierarchy",
+                       "Analyze types in module: " << M.getModuleIdentifier());
+  // store analyzed module
+  VisitedModules.insert(&M);
+
+  // TODO (max): go over all metadata nodes and construct hierarchy
+}
+
+std::string
+LLVMTypeHierarchy_MD::removeStructOrClassPrefix(const llvm::StructType &T) {
+  return removeStructOrClassPrefix(T.getName().str());
+}
+
+std::string
+LLVMTypeHierarchy_MD::removeStructOrClassPrefix(const std::string &TypeName) {
+  llvm::StringRef SR(TypeName);
+  if (SR.startswith(StructPrefix)) {
+    return SR.drop_front(StructPrefix.size()).str();
+  }
+  if (SR.startswith(ClassPrefix)) {
+    return SR.drop_front(ClassPrefix.size()).str();
+  }
+  return TypeName;
+}
+
+std::set<const llvm::StructType *>
+LLVMTypeHierarchy_MD::getSubTypes(const llvm::StructType *Type) {
+
+  // TODO (max): ask fabian how to implement boostless version of TypeVertexMap
+
+  // if (TypeVertexMap.count(Type)) {
+  //   return TypeGraph[TypeVertexMap[Type]].ReachableTypes;
+  // }
+  return {};
+}
+
+std::set<const llvm::StructType *>
+LLVMTypeHierarchy_MD::getSuperTypes(const llvm::StructType * /*Type*/) {
+  std::set<const llvm::StructType *> ReachableTypes;
+  // TODO (philipp): what does this function do?
+  return ReachableTypes;
+}
+
+const llvm::StructType *
+LLVMTypeHierarchy_MD::getType(std::string TypeName) const {
+  for (const auto &Vertex : TypeGraph.Vertices) {
+    if (Vertex.Type->getName() == TypeName) {
+      return Vertex.Type;
+    }
+  }
+  return nullptr;
+}
+
+std::set<const llvm::StructType *> LLVMTypeHierarchy_MD::getAllTypes() const {
+  std::set<const llvm::StructType *> Types;
+  for (const auto &Vertex : TypeGraph.Vertices) {
+    Types.insert(Vertex.Type);
+  }
+  return Types;
+}
+
+std::string
+LLVMTypeHierarchy_MD::getTypeName(const llvm::StructType *Type) const {
+  return Type->getStructName().str();
+}
+
+bool LLVMTypeHierarchy_MD::hasVFTable(const llvm::StructType *Type) const {
+  if (TypeVFTMap.count(Type)) {
+    return !TypeVFTMap.at(Type).empty();
+  }
+  return false;
+}
+
+const LLVMVFTable *
+LLVMTypeHierarchy_MD::getVFTable(const llvm::StructType *Type) const {
+  if (TypeVFTMap.count(Type)) {
+    return &TypeVFTMap.at(Type);
+  }
+  return nullptr;
 }
 
 void LLVMTypeHierarchy_MD::getMetaData(llvm::Function &F) {
