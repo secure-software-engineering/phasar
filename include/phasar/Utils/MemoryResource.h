d9743e64ef9c447ad4ca1ca314d1420001fa68be
Martin Musiol <mmusiol@mail.uni-paderborn.de>
2023-07-06 18:35:35 +0200

    Reverted wrong Header Guard

--- include/phasar/Utils/MemoryResource.h
+++ include/phasar/Utils/MemoryResource.h
@@ -1,11 +1,11 @@
 /// On some MAC systems, <memory_resource> is still not fully implemented, so do
 /// a workaround here
 
-#ifndef PHASAR_UTILS_MEMORYRESOURCE_H
+#ifndef HAS_MEMORY_RESOURCE
 #if !defined(__has_include) || __has_include(<memory_resource>)
-#define PHASAR_UTILS_MEMORYRESOURCE_H 1
+#define HAS_MEMORY_RESOURCE 1
 #include <memory_resource>
 #else
-#define PHASAR_UTILS_MEMORYRESOURCE_H 0
+#define HAS_MEMORY_RESOURCE 0
 #endif
 #endif
