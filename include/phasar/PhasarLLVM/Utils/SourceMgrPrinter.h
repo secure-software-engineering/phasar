b874c0564234862c18fc4138c56e7a8ab9095fe1
SanthoshMohan <santhoshmohan0897@gmail.com>
2024-01-09 13:40:37 +0100

    Testcase for SourceMgrPrinter

--- include/phasar/PhasarLLVM/Utils/SourceMgrPrinter.h
+++ include/phasar/PhasarLLVM/Utils/SourceMgrPrinter.h
@@ -3,27 +3,33 @@
 #include "phasar/PhasarLLVM/Utils/AnalysisPrinterBase.h"
 #include "phasar/PhasarLLVM/Utils/LLVMIRToSrc.h"
 #include "phasar/PhasarLLVM/Utils/OnTheFlyAnalysisPrinter.h"
+#include "phasar/Utils/Logger.h"
 
 #include "llvm/Support/MemoryBuffer.h"
 
 #include <llvm/ADT/StringMap.h>
 #include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/raw_ostream.h>
 
 namespace psr {
 template <typename AnalysisDomainTy>
-class SourceMgrPrinter : public OnTheFlyAnalysisPrinter<AnalysisDomainTy> {
+class SourceMgrPrinter : public AnalysisPrinterBase<AnalysisDomainTy> {
 public:
   SourceMgrPrinter() = default;
+  SourceMgrPrinter<AnalysisDomainTy>(llvm::raw_ostream &OS) : OS(OS){};
 
   void onInitialize() override {}
   void onFinalize() const override{};
   void onResult(Warning<AnalysisDomainTy> Warn) override {
-
-    SrcMgr.PrintMessage(SrcMgr.FindLocForLineAndColumn(
-                            getSourceBufId(getFilePathFromIR(Warn.Fact)),
-                            getLineAndColFromIR(Warn.Fact).first,
-                            getLineAndColFromIR(Warn.Fact).second),
-                        llvm::SourceMgr::DK_Warning, Msg);
+    auto BufIdOpt = getSourceBufId(getFilePathFromIR(Warn.Instr));
+    if (BufIdOpt.has_value()) {
+      SrcMgr.PrintMessage(OS,
+                          SrcMgr.FindLocForLineAndColumn(
+                              BufIdOpt.value(),
+                              getLineAndColFromIR(Warn.Instr).first,
+                              getLineAndColFromIR(Warn.Instr).second),
+                          llvm::SourceMgr::DK_Warning, Msg);
+    }
   }
 
   std::optional<unsigned> getSourceBufId(llvm::StringRef FileName) {
@@ -33,7 +39,7 @@ public:
 
     auto Buf = llvm::MemoryBuffer::getFile(FileName, true);
     if (!Buf) {
-      FileNameIDMap.try_emplace(FileName, std::nullopt);
+      PHASAR_LOG_LEVEL(WARNING, "File not accessible: " << FileName);
       return std::nullopt;
     }
 
@@ -46,6 +52,7 @@ private:
   llvm::SourceMgr SrcMgr;
   llvm::StringMap<unsigned> FileNameIDMap;
   static constexpr llvm::StringLiteral Msg = "Phasar found an error";
+  llvm::raw_ostream &OS = llvm::errs();
 };
 } // namespace psr
 #endif
---
+++ unittests/Utils/SourceMgrPrinterTest.cpp
@@ -0,0 +1,107 @@
+#include "phasar/PhasarLLVM/Utils/SourceMgrPrinter.h"
+
+#include "phasar/DataFlow/IfdsIde/Solver/IFDSSolver.h"
+#include "phasar/PhasarLLVM/ControlFlow/LLVMBasedICFG.h"
+#include "phasar/PhasarLLVM/DataFlow/IfdsIde/Problems/IFDSUninitializedVariables.h"
+#include "phasar/PhasarLLVM/HelperAnalyses.h"
+#include "phasar/PhasarLLVM/SimpleAnalysisConstructor.h"
+#include "phasar/PhasarLLVM/Utils/AnalysisPrinterBase.h"
+
+#include "TestConfig.h"
+#include "gtest/gtest.h"
+
+#include <set>
+
+#include <llvm/ADT/StringRef.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace psr;
+
+class GroundTruthCollector
+    : public SourceMgrPrinter<LLVMIFDSAnalysisDomainDefault> {
+public:
+  // constructor init Groundtruth in each fixture
+  GroundTruthCollector(std::set<std::string> &GroundTruth)
+      : SourceMgrPrinter<LLVMIFDSAnalysisDomainDefault>(OS),
+        GroundTruth(GroundTruth), OS(Str){};
+
+  std::string findSubString(const std::string &String) {
+    size_t LastSlashPos = String.rfind('/');
+    if (LastSlashPos != std::string::npos) {
+      // Find the first space after the last '/'
+      size_t FirstSpacePos = String.find(' ', LastSlashPos);
+
+      if (FirstSpacePos != std::string::npos) {
+        // Extract the substring from the last '/' to the first space after
+        // the last '/'
+        std::string Result =
+            String.substr(LastSlashPos + 1, FirstSpacePos - LastSlashPos - 2);
+        return Result;
+      }
+      return "";
+    }
+    return "";
+  }
+
+  void findAndRemove(const std::string &SearchString) {
+    std::string SubString = findSubString(SearchString);
+    GroundTruth.erase(SubString);
+  }
+
+  void onResult(Warning<LLVMIFDSAnalysisDomainDefault> Warn) override {
+    SourceMgrPrinter<LLVMIFDSAnalysisDomainDefault>::onResult(Warn);
+    findAndRemove(OS.str());
+  }
+
+  void onFinalize() const override { EXPECT_TRUE(GroundTruth.empty()); }
+
+private:
+  std::set<std::string> GroundTruth{};
+  std::string Str;
+  llvm::raw_string_ostream OS;
+};
+
+class SourceMgrPrinterTest : public ::testing::Test {
+protected:
+  static constexpr auto PathToLlFiles =
+      PHASAR_BUILD_SUBFOLDER("uninitialized_variables/");
+
+  const std::vector<std::string> EntryPoints = {"main"};
+  std::optional<IFDSUninitializedVariables> UnInitProblem;
+  std::optional<HelperAnalyses> HA;
+
+  void initialize(const llvm::Twine &IRFile) {
+    HA.emplace(PathToLlFiles + IRFile, EntryPoints);
+    UnInitProblem =
+        createAnalysisProblem<IFDSUninitializedVariables>(*HA, EntryPoints);
+  }
+
+  void doAnalysisTest(
+      llvm::StringRef IRFile,
+      AnalysisPrinterBase<LLVMIFDSAnalysisDomainDefault> &GTPrinter) {
+    initialize(IRFile);
+    UnInitProblem->setAnalysisPrinter(&GTPrinter);
+    IFDSSolver Solver(*UnInitProblem, &HA->getICFG());
+    Solver.solve();
+    GTPrinter.onFinalize();
+  }
+};
+
+/* ============== BASIC TESTS ============== */
+
+TEST_F(SourceMgrPrinterTest, UninitTest_01_LEAK) {
+
+  std::set<std::string> GroundTruth;
+
+  GroundTruth = {"binop_uninit.cpp:3:11", "binop_uninit.cpp:3:13"};
+
+  GroundTruthCollector GroundTruthPrinter = {GroundTruth};
+
+  doAnalysisTest("binop_uninit_cpp_dbg.ll", GroundTruthPrinter);
+}
+
+// main function for the test case
+int main(int Argc, char **Argv) {
+  ::testing::InitGoogleTest(&Argc, Argv);
+  return RUN_ALL_TESTS();
+}
