aa206e30ce1ed81da3390935b9ae030285722e90
Fabian Schiebel <52407375+fabianbs96@users.noreply.github.com>
2024-04-25 18:53:53 +0200

    Make the docker push pipeline work again

--- utils/InstallAptDependencies.sh
+++ utils/InstallAptDependencies.sh
@@ -3,4 +3,4 @@ set -e
 
 sudo apt-get update
 sudo apt-get install git -y
-sudo apt-get install zlib1g-dev python3 g++ ninja-build cmake -y
+sudo apt-get install zlib1g-dev python3 python3-pip g++ ninja-build cmake -y
