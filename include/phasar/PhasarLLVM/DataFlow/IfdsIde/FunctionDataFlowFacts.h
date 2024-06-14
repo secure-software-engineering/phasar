e2b964537b1719cb57ff564ee0581a4cf3a497e7
bulletSpace <erik.binder@hotmail.com>
2024-06-14 13:38:32 +0200

    data structure update

--- include/phasar/PhasarLLVM/DataFlow/IfdsIde/FunctionDataFlowFacts.h
+++ include/phasar/PhasarLLVM/DataFlow/IfdsIde/FunctionDataFlowFacts.h
@@ -1,25 +1,43 @@
 #include <unordered_map>
 #include <set>
 #include <string>
-#include <iterator>
+#include <variant>
 
-using namespace std;
+struct DataFlowFact {
+    std::variant<int, long, float, double, char, std::string> fact;
+};
 
 class FunctionDataFlowFacts {
     private :
-        unordered_map<string, set<string>> fdff;
+        std::unordered_map<std::string, std::unordered_map<int, std::set<DataFlowFact>>> fdff;
 
     public :
         FunctionDataFlowFacts ();
 
-        void insert (string funcKey, set<string> dff) {
-            this->fdff.insert ({funcKey, dff});
+        void insert (std::string funcKey, int index, std::set<DataFlowFact> dff) {
+            const auto it = this->fdff.find (funcKey);
+            if (it == this->fdff.end()) {
+                std::unordered_map<int, std::set<DataFlowFact>> new_facts;
+                new_facts.insert ({index, dff});
+                this->fdff.insert ({funcKey, new_facts});
+            }
+            
+            else {
+                it->second.insert ({index, dff});
+            }
         }
 
-        void remove (string funcKey) {
-            this->fdff.erase (funcKey);
+        const std::unordered_map<int, std::set<DataFlowFact>>& getDataFlowFactsOf (std::string funcKey) const {
+            auto const it = this->fdff.find (funcKey);
+            if (it == this->fdff.end()) {
+                std::unordered_map<int, std::set<DataFlowFact>> ret;
+                return ret;
+            }
+
+            else {
+                return it->second;
+            }
         }
-        set<string> getDataFlowFacts (string funcKey) {
-            return this->fdff.find (funcKey)->second;
-        }
+
+        //method to get num of parameters/flow facts
 };
