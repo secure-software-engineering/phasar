a168c64a9252c3ed33ea0a6b404ece68f2b03f58
mxHuber <huber.maximilian.leo@gmail.com>
2025-02-25 18:28:49 +0100

    fixed some NoAliasInfoTabProb code

--- include/phasar/PhasarLLVM/DataFlow/IfdsIde/IDENoAliasInfoTabulationProblem.h
+++ include/phasar/PhasarLLVM/DataFlow/IfdsIde/IDENoAliasInfoTabulationProblem.h
@@ -52,58 +52,65 @@ public:
 
   FlowFunctionPtrType getNormalFlowFunction(n_t Curr, n_t /*Succ*/) override {
     if (const auto *Load = llvm::dyn_cast<llvm::LoadInst>(Curr)) {
-// TODO: fix code below
-#if false
-      return generateFlowIf(Load, [Load](d_t Source) {
+      // TODO: fix code below
+      // #if false
+      return this->generateFlowIf(Load, [Load](d_t Source) {
         return Source == Load->getPointerOperand();
       });
-#endif
+      // #endif
     }
     if (const auto *Store = llvm::dyn_cast<llvm::StoreInst>(Curr)) {
       return strongUpdateStore(Store);
     }
     if (const auto *UnaryOp = llvm::dyn_cast<llvm::UnaryOperator>(Curr)) {
-// TODO: fix code below
-#if false
-      return generateFlow(UnaryOp, UnaryOp->getOperand(0));
-#endif
+      // TODO: fix code below
+      // #if false
+      return this->generateFlow(UnaryOp, UnaryOp->getOperand(0));
+      // #endif
     }
     if (const auto *BinaryOp = llvm::dyn_cast<llvm::BinaryOperator>(Curr)) {
-// TODO: fix code below
+      // TODO: fix code below. How do we get the Operands of BinaryOperators?
 #if false
-      return generateFlowIf(BinaryOp, [BinaryOp](d_t Source) {
+      return this->generateFlowIf(BinaryOp, [BinaryOp](d_t Source) {
         return Source == BinaryOp->getLeftOp() ||
                Source == BinaryOp->getRightOp();
       });
 #endif
     }
     if (const auto *GetElementPtr = llvm::dyn_cast<llvm::GEPOperator>(Curr)) {
-// TODO: fix code below
-#if false
-      return generateFlow(GetElementPtr, GetElementPtr->getPointerOperand());
-#endif
+      // TODO: fix code below
+      // #if false
+      return this->generateFlow(GetElementPtr,
+                                GetElementPtr->getPointerOperand());
+      // #endif
     }
   }
   FlowFunctionPtrType getCallFlowFunction(n_t CallInst,
                                           f_t CalleeFun) override {
-// TODO: fix code below
-#if false
-    return mapFactsToCallee(CallInst, CalleeFun, [](d_t Actual, d_t Source) {
-      return Actual == Source && Actual->getType()->isPointerTy();
-    });
-#endif
+    // TODO: fix code below
+    // #if false
+    if (const auto *CallSite =
+            llvm::dyn_cast_or_null<llvm::CallBase>(CallInst)) {
+      return mapFactsToCallee(CallSite, CalleeFun, [](d_t Actual, d_t Source) {
+        return Actual == Source && Actual->getType()->isPointerTy();
+      });
+      // #endif
+    }
   }
   FlowFunctionPtrType getRetFlowFunction(n_t CallSite, f_t /*CalleeFun*/,
                                          n_t ExitInst,
                                          n_t /*RetSite*/) override {
-// TODO: fix code below
-#if false
-    return mapFactsToCaller(llvm::cast<llvm::CallBase>(CallSite), ExitInst,
-                            [](d_t Param, d_t Source) {
-                              return Param == Source &&
-                                     Param->getType()->isPointerTy();
-                            });
-#endif
+    // TODO: fix code below
+    // #if false
+    if (const auto *ConfirmedCallSite =
+            llvm::dyn_cast_or_null<llvm::CallBase>(CallSite)) {
+      return mapFactsToCaller(llvm::cast<llvm::CallBase>(ConfirmedCallSite),
+                              ExitInst, [](d_t Param, d_t Source) {
+                                return Param == Source &&
+                                       Param->getType()->isPointerTy();
+                              });
+    }
+    // #endif
   }
   FlowFunctionPtrType
   getCallToRetFlowFunction(n_t CallSite, n_t /*RetSite*/,
