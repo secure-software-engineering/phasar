bf88bd9c21b270ca4dd0114101c345ff75ee1d18
Fabian Schiebel <fabianbs@mail.upb.de>
2025-07-20 12:40:46 +0200

    Fix compilation of IDEVarTabulationProblemTest

--- unittests/PhasarLLVM/DataFlow/IfdsIde/Variability/IDEVarTabulationProblemTest.cpp
+++ unittests/PhasarLLVM/DataFlow/IfdsIde/Variability/IDEVarTabulationProblemTest.cpp
@@ -7,90 +7,75 @@
  *     Fabian Schiebel, Philipp Schubert and others
  *****************************************************************************/
 
-#include <limits>
-#include <tuple>
-#include <utility>
+#include "phasar/DataFlow/IfdsIde/IDEVarTabulationProblem.h"
 
-#include "gtest/gtest.h"
+#include "phasar/DataFlow/IfdsIde/Solver/IDESolver.h"
+#include "phasar/Domain/LatticeDomain.h"
+#include "phasar/PhasarLLVM/ControlFlow/LLVMBasedVarICFG.h"
+#include "phasar/PhasarLLVM/DB/LLVMProjectIRDB.h"
+#include "phasar/PhasarLLVM/DataFlow/IfdsIde/Problems/IDELinearConstantAnalysis.h"
+#include "phasar/PhasarLLVM/Utils/LLVMShorthands.h"
+#include "phasar/Utils/Logger.h"
 
 #include "llvm/ADT/StringRef.h"
 
-#include "phasar/DB/ProjectIRDB.h"
-#include "phasar/PhasarLLVM/ControlFlow/LLVMBasedVarICFG.h"
-#include "phasar/PhasarLLVM/DataFlowSolver/IfdsIde/IDEVarTabulationProblem.h"
-#include "phasar/PhasarLLVM/DataFlowSolver/IfdsIde/Problems/IDELinearConstantAnalysis.h"
-#include "phasar/PhasarLLVM/DataFlowSolver/IfdsIde/Solver/IDESolver.h"
-#include "phasar/PhasarLLVM/Passes/ValueAnnotationPass.h"
-#include "phasar/PhasarLLVM/Pointer/LLVMPointsToSet.h"
-#include "phasar/PhasarLLVM/TypeHierarchy/LLVMTypeHierarchy.h"
-#include "phasar/Utils/LLVMShorthands.h"
-#include "phasar/Utils/Logger.h"
-
 #include "TestConfig.h"
+#include "gtest/gtest.h"
+
+#include <limits>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
 using namespace psr;
 
 /* ============== TEST FIXTURE ============== */
 class IDEVarTabulationProblemTest : public ::testing::Test {
 protected:
-  const std::string PathToLLFiles =
-      unittest::PathToLLTestFiles +
-      "/variability/linear_constant/manually_transformed/";
-  const std::set<std::string> EntryPoints = {"main"};
+  static constexpr auto PathToLLFiles = PHASAR_BUILD_SUBFOLDER(
+      "variability/linear_constant/manually_transformed/");
+  const std::vector<std::string> EntryPoints = {"main"};
 
+  using ResultSetTy = std::set<std::pair<std::string, LatticeDomain<int64_t>>>;
   // Function - Line Nr - Variable - Z3Constraint x Value
   using LCAVarCompactResults_t =
-      std::tuple<std::string, std::size_t, std::string,
-                 std::set<std::pair<std::string, int64_t>>>;
-
-  ProjectIRDB *IRDB = nullptr;
-
-  void SetUp() override { boost::log::core::get()->set_logging_enabled(false); }
-
-  const int64_t TOP = std::numeric_limits<int64_t>::min();
-
-  const int64_t BOTTOM = std::numeric_limits<int64_t>::max();
+      std::tuple<std::string, std::size_t, std::string, ResultSetTy>;
 
   // IDELinearConstantAnalysis::lca_restults_t
   void
-  doAnalysisAndCompareResults(const std::string &llvmFilePath,
+  doAnalysisAndCompareResults(const std::string &IRFilePath,
                               std::set<LCAVarCompactResults_t> &GroundTruth,
-                              bool printDump = false) {
-    IRDB = new ProjectIRDB({PathToLLFiles + llvmFilePath}, IRDBOptions::WPA);
-    if (printDump) {
-      IRDB->emitPreprocessedIR(std::cout, false);
+                              bool PrintDump = false) {
+    LLVMProjectIRDB IRDB(PathToLLFiles + IRFilePath);
+    if (PrintDump) {
+      IRDB.emitPreprocessedIR(llvm::outs());
     }
-    ValueAnnotationPass::resetValueID();
-    LLVMTypeHierarchy TH(*IRDB);
-    LLVMPointsToSet PT(*IRDB);
-    LLVMBasedVarICFG VICFG(*IRDB, CallGraphAnalysisType::OTF, EntryPoints, &TH,
-                           &PT);
-    IDELinearConstantAnalysis LCAProblem(IRDB, &TH, &VICFG, &PT, EntryPoints);
-    IDEVarTabulationProblem_P<IDELinearConstantAnalysis> VARAProblem(LCAProblem,
-                                                                     VICFG);
-    IDESolver_P<IDEVarTabulationProblem_P<IDELinearConstantAnalysis>> LCASolver(
-        VARAProblem);
-    // LCASolver.enableESGAsDot();
+
+    LLVMBasedICFG ICFG(&IRDB, CallGraphAnalysisType::OTF, EntryPoints);
+    IDELinearConstantAnalysis LCAProblem(&IRDB, &ICFG, EntryPoints);
+    IDEVarTabulationProblem VARAProblem(LCAProblem, ICFG);
+    IDESolver LCASolver(&VARAProblem, &ICFG);
+
     LCASolver.solve();
-    if (printDump) {
+    if (PrintDump) {
       LCASolver.dumpResults();
     }
-    // std::ofstream OFS(llvmFilePath + "-ESG.dot");
-    // LCASolver.emitESGAsDot(OFS);
-    for (auto &Truth : GroundTruth) {
-      auto Fun = IRDB->getFunctionDefinition(std::get<0>(Truth));
-      auto Inst = getNthInstruction(Fun, std::get<1>(Truth));
+
+    for (const auto &[TruthFnName, TruthInstId, TruthVarName, TruthResultSet] :
+         GroundTruth) {
+      const auto *Fun = IRDB.getFunctionDefinition(TruthFnName);
+      const auto *Inst = getNthInstruction(Fun, TruthInstId);
       auto Results = LCASolver.resultsAt(Inst);
       for (auto &[Fact, Value] : Results) {
-        if (llvm::StringRef(llvmIRToString(Fact))
-                .startswith(std::get<2>(Truth))) {
+        if (llvm::StringRef(llvmIRToString(Fact)).startswith(TruthVarName)) {
           for (auto &[Constraint, IntegerValue] : Value) {
             bool Found = false;
-            for (auto &[TrueConstaint, TrueIntegerValue] : std::get<3>(Truth)) {
-              // std::cout << "Comparing: " << Constraint.to_string() << " and "
-              // << TrueConstaint << '\n';
+            for (const auto &[TrueConstaint, TrueIntegerValue] :
+                 TruthResultSet) {
+
               if (Constraint.to_string() == TrueConstaint) {
-                EXPECT_TRUE(IntegerValue == TrueIntegerValue);
+                EXPECT_EQ(IntegerValue, TrueIntegerValue);
                 Found = true;
                 break;
               }
@@ -104,8 +89,6 @@ protected:
       }
     }
   }
-
-  void TearDown() override { delete IRDB; }
 
 }; // Test Fixture
 
@@ -122,85 +105,72 @@ protected:
 TEST_F(IDEVarTabulationProblemTest, HandleBasic_01) {
   std::set<LCAVarCompactResults_t> GroundTruth;
   GroundTruth.emplace("main", 11, "x",
-                      std::set<std::pair<std::string, int64_t>>{
-                          {"A_defined", 42}, {"(not A_defined)", 13}});
-  GroundTruth.emplace("main", 11, "retval",
-                      std::set<std::pair<std::string, int64_t>>{{"true", 0}});
+                      ResultSetTy{{"A_defined", 42}, {"(not A_defined)", 13}});
+  GroundTruth.emplace("main", 11, "retval", ResultSetTy{{"true", 0}});
   doAnalysisAndCompareResults("basic_01_c.ll", GroundTruth, true);
 }
 
 TEST_F(IDEVarTabulationProblemTest, HandleBasic_02) {
   std::set<LCAVarCompactResults_t> GroundTruth;
-  GroundTruth.emplace("main", 6, "x",
-                      std::set<std::pair<std::string, int64_t>>{{"true", 150}});
-  GroundTruth.emplace("main", 6, "%0",
-                      std::set<std::pair<std::string, int64_t>>{{"true", 150}});
-  GroundTruth.emplace("main", 6, "retval",
-                      std::set<std::pair<std::string, int64_t>>{{"true", 0}});
+  GroundTruth.emplace("main", 6, "x", ResultSetTy{{"true", 150}});
+  GroundTruth.emplace("main", 6, "%0", ResultSetTy{{"true", 150}});
+  GroundTruth.emplace("main", 6, "retval", ResultSetTy{{"true", 0}});
   doAnalysisAndCompareResults("basic_02_c.ll", GroundTruth, false);
 }
 
 TEST_F(IDEVarTabulationProblemTest, HandleBasic_03) {
   std::set<LCAVarCompactResults_t> GroundTruth;
   GroundTruth.emplace("main", 14, "x",
-                      std::set<std::pair<std::string, int64_t>>{
-                          {"A_defined", 42}, {"(not A_defined)", 13}});
-  GroundTruth.emplace("main", 14, "y",
-                      std::set<std::pair<std::string, int64_t>>{{"true", 210}});
-  GroundTruth.emplace("main", 14, "retval",
-                      std::set<std::pair<std::string, int64_t>>{{"true", 0}});
+                      ResultSetTy{{"A_defined", 42}, {"(not A_defined)", 13}});
+  GroundTruth.emplace("main", 14, "y", ResultSetTy{{"true", 210}});
+  GroundTruth.emplace("main", 14, "retval", ResultSetTy{{"true", 0}});
   doAnalysisAndCompareResults("basic_03_c.ll", GroundTruth, false);
 }
 
 TEST_F(IDEVarTabulationProblemTest, HandleBasic_04) {
   std::set<LCAVarCompactResults_t> GroundTruth;
-  GroundTruth.emplace("main", 9, "x",
-                      std::set<std::pair<std::string, int64_t>>{{"true", 301}});
-  GroundTruth.emplace("main", 9, "retval",
-                      std::set<std::pair<std::string, int64_t>>{{"true", 0}});
+  GroundTruth.emplace("main", 9, "x", ResultSetTy{{"true", 301}});
+  GroundTruth.emplace("main", 9, "retval", ResultSetTy{{"true", 0}});
   doAnalysisAndCompareResults("basic_03_c.ll", GroundTruth, false);
 }
 
 TEST_F(IDEVarTabulationProblemTest, HandleLoops_01) {
   std::set<LCAVarCompactResults_t> GroundTruth;
-  GroundTruth.emplace(
-      "main", 24, "x",
-      std::set<std::pair<std::string, int64_t>>{
-          {"true", TOP}, {"A_defined", BOTTOM}, {"(not A_defined)", 0}});
-  GroundTruth.emplace(
-      "main", 24, "i",
-      std::set<std::pair<std::string, int64_t>>{
-          {"true", TOP}, {"A_defined", BOTTOM}, {"(not A_defined)", BOTTOM}});
+  GroundTruth.emplace("main", 24, "x",
+                      ResultSetTy{{"true", Top{}},
+                                  {"A_defined", Bottom{}},
+                                  {"(not A_defined)", 0}});
+  GroundTruth.emplace("main", 24, "i",
+                      ResultSetTy{{"true", Top{}},
+                                  {"A_defined", Bottom{}},
+                                  {"(not A_defined)", Bottom{}}});
   GroundTruth.emplace(
       "main", 24, "retval",
-      std::set<std::pair<std::string, int64_t>>{
-          {"true", TOP}, {"A_defined", 0}, {"(not A_defined)", 0}});
+      ResultSetTy{{"true", Top{}}, {"A_defined", 0}, {"(not A_defined)", 0}});
   doAnalysisAndCompareResults("loop_01_c.ll", GroundTruth, false);
 }
 
 TEST_F(IDEVarTabulationProblemTest, HandleCalls_01) {
   std::set<LCAVarCompactResults_t> GroundTruth;
-  GroundTruth.emplace(
-      "main", 13, "x",
-      std::set<std::pair<std::string, int64_t>>{
-          {"true", TOP}, {"A_defined", 100}, {"(not A_defined)", 99}});
+  GroundTruth.emplace("main", 13, "x",
+                      ResultSetTy{{"true", Top{}},
+                                  {"A_defined", 100},
+                                  {"(not A_defined)", 99}});
   GroundTruth.emplace(
       "main", 13, "retval",
-      std::set<std::pair<std::string, int64_t>>{
-          {"true", TOP}, {"A_defined", 0}, {"(not A_defined)", 0}});
+      ResultSetTy{{"true", Top{}}, {"A_defined", 0}, {"(not A_defined)", 0}});
   doAnalysisAndCompareResults("call_01_c.ll", GroundTruth, false);
 }
 
 TEST_F(IDEVarTabulationProblemTest, HandleRecursion_01) {
   std::set<LCAVarCompactResults_t> GroundTruth;
-  GroundTruth.emplace(
-      "main", 13, "x",
-      std::set<std::pair<std::string, int64_t>>{
-          {"true", TOP}, {"A_defined", BOTTOM}, {"(not A_defined)", 5}});
+  GroundTruth.emplace("main", 13, "x",
+                      ResultSetTy{{"true", Top{}},
+                                  {"A_defined", Bottom{}},
+                                  {"(not A_defined)", 5}});
   GroundTruth.emplace(
       "main", 13, "retval",
-      std::set<std::pair<std::string, int64_t>>{
-          {"true", TOP}, {"A_defined", 0}, {"(not A_defined)", 0}});
+      ResultSetTy{{"true", Top{}}, {"A_defined", 0}, {"(not A_defined)", 0}});
   doAnalysisAndCompareResults("recursion_01_c.ll", GroundTruth, false);
 }
 
