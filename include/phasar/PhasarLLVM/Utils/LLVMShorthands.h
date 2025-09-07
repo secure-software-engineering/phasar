abeb431be152b064a783d6768553e1cefce1b9b1
Fabian Schiebel <fabian.schiebel@iem.fraunhofer.de>
2025-09-07 14:14:09 +0200

    Implement allocated-types collection in terms of debug info

--- include/phasar/PhasarLLVM/Utils/LLVMShorthands.h
+++ include/phasar/PhasarLLVM/Utils/LLVMShorthands.h
@@ -104,6 +104,14 @@ std::string llvmIRToShortString(const llvm::Value *V);
 [[nodiscard]] std::string llvmTypeToString(const llvm::Type *Ty,
                                            bool Shorten = false);
 
+/**
+ * @brief Returns a string-representation of a LLVM Debug-Info type.
+ *
+ * @param Shorten Tries to shorten the output
+ */
+[[nodiscard]] std::string llvmTypeToString(const llvm::DIType *Ty,
+                                           bool Shorten = false);
+
 LLVM_DUMP_METHOD void dumpIRValue(const llvm::Value *V);
 LLVM_DUMP_METHOD void dumpIRValue(const llvm::Instruction *V);
 LLVM_DUMP_METHOD void dumpIRValue(const llvm::Function *V);
