1f683ea7b4534d30d23315d46fe90412bf75cb74
mxHuber <huber.maximilian.leo@gmail.com>
2025-10-23 23:19:38 +0200

    basic ci impl, lib coverage, fixed release cmake error

--- cmake/phasar_macros.cmake
+++ cmake/phasar_macros.cmake
@@ -36,7 +36,9 @@ function(add_phasar_unittest test_name)
   set_tests_properties("${test}" PROPERTIES LABELS "all")
   set(CTEST_OUTPUT_ON_FAILURE ON)
 
-  target_code_coverage(${test} AUTO ALL)
+  if (NOT CMAKE_BUILD_TYPE STREQUAL "Release")
+    target_code_coverage(${test} AUTO ALL)
+  endif()
 endfunction()
 
 function(validate_binary_version result item)
@@ -353,6 +355,10 @@ function(add_phasar_library name)
   endif()
 
   set_property(GLOBAL APPEND PROPERTY LLVM_EXPORTS ${name})
+
+  if (NOT CMAKE_BUILD_TYPE STREQUAL "Release")
+    target_code_coverage(${name} AUTO ALL)
+  endif()
 endfunction(add_phasar_library)
 
 macro(subdirlist result curdir)
