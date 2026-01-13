294f73093c14606c8933ea8972e0a3d3cb0640eb
mxHuber <huber.maximilian.leo@gmail.com>
2026-01-13 11:20:43 +0100

    renamed tests

--- test/llvm_test_code/pointers/context_05.c
+++ test/llvm_test_code/pointers/context_05.c
@@ -1,15 +1,14 @@
 
-int *buzz(int *s) { return s; }
-int *baz(int *r) { return buzz(r); }
-int *bar(int *q) { return baz(q); }
-int *foo(int *p) { return bar(p); }
+int *id1(int *p) { return p; }
+int *id2(int *q) { return id1(q); }
+int *id3(int *r) { return id2(r); }
+int *id4(int *s) { return id3(s); }
 
 int main() {
   int x = 42;
-  int y = 43;
 
-  int *xx = foo(&x);
-  int *yy = foo(&y);
+  int *xx = id4(&x);
+  int *yy = id4(&x);
 
   return *xx;
 }
